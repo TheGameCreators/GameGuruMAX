ae7b94a29e831f89f929800ed6bd466ef5eb8da9
Lee Bamber <35840583+LeeBamberTGC@users.noreply.github.com>
2023-02-19 15:52:50 +0000

    POM
    * Refined POM and added ability to change height texture in custom material

--- GameGuru Core/GameGuru/Source/M-MapFile.cpp
+++ GameGuru Core/GameGuru/Source/M-MapFile.cpp
@@ -2630,6 +2630,7 @@ void mapfile_collectfoldersandfiles ( cstr levelpathfolder )
 						addtocollection(material.normalMapName[i].Get());
 						addtocollection(material.emissiveMapName[i].Get());
 						addtocollection(material.surfaceMapName[i].Get());
+						addtocollection(material.displacementMapName[i].Get());
 					}
 				}
 			}

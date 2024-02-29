c958b80536e4384cb8a375cf6fb2d6b05fed2fc6
Steve <95015657+Necrym59@users.noreply.github.com>
2024-02-29 16:06:18 +1100

    Script Updates

--- Scripts/scriptbank/rpg/examine.lua
+++ Scripts/scriptbank/rpg/examine.lua
@@ -1,11 +1,16 @@
--- Examine v6: by Necrym59
--- DESCRIPTION: [PICKUP_RANGE=100]
+-- Examine v7: by Necrym59
+-- DESCRIPTION: Allows to examine an object.
+-- DESCRIPTION: [PICKUP_RANGE=90(0,100)]
 -- DESCRIPTION: [PICKUP_MESSAGE$="E to Examine object"]
 -- DESCRIPTION: [EXAMINE_MESSAGE$="Hmmm..."]
 -- DESCRIPTION: [EXAMINE_SPEED=50]
+-- DESCRIPTION: [@PROMPT_DISPLAY=1(1=Local,2=Screen)]
 
-U = require "scriptbank\\utillib"
-Q = require "scriptbank\\quatlib"
+local module_misclib = require "scriptbank\\module_misclib"
+local U = require "scriptbank\\utillib"
+local Q = require "scriptbank\\quatlib"
+g_tEnt = {}
+
 local rad = math.rad
 local deg = math.deg
 local abs = math.abs
@@ -15,6 +20,7 @@ local pickup_range			= {}
 local pickup_message		= {}
 local examine_message		= {}
 local examine_speed			= {}
+local prompt_display 		= {}
 
 local exminetime		= {}
 local status 			= {}
@@ -33,12 +39,13 @@ local prop_z 			= {}
 local prop_h 			= {}	
 local last_gun			= {}
 
-function examine_properties(e, pickup_range, pickup_message, examine_message, examine_speed)
+function examine_properties(e, pickup_range, pickup_message, examine_message, examine_speed, prompt_display)
 	examine[e] = g_Entity[e]
 	examine[e].pickup_range = pickup_range
 	examine[e].pickup_message =  pickup_message
 	examine[e].examine_message = examine_message
 	examine[e].examine_speed = examine_speed
+	examine[e].prompt_display = prompt_display
 end 
 
 function examine_init(e)
@@ -46,9 +53,14 @@ function examine_init(e)
 	examine[e].pickup_range = 100
 	examine[e].pickup_message = "E to Examine object"
 	examine[e].examine_message = "Hmmm..."
-	examine[e].examine_speed = 50	
+	examine[e].examine_speed = 50
+	examine[e].prompt_display = 1
+	
 	status[e] = "init"
 	exminetime[e] = 0
+	g_tEnt = 0
+	tEnt[e] = 0
+	selectobj[e] = 0
 	last_gun[e] = g_PlayerGunName
 end 
 
@@ -70,28 +82,14 @@ function examine_main(e)
 	
 	if status[e] == "pick up" then 	
 		if PlayerDist < examine[e].pickup_range then			
-			-- pinpoint select object--			
-			local px, py, pz = GetCameraPositionX(0), GetCameraPositionY(0), GetCameraPositionZ(0)
-			local rayX, rayY, rayZ = 0,0,examine[e].pickup_range
-			local paX, paY, paZ = math.rad(GetCameraAngleX(0)), math.rad(GetCameraAngleY(0)), math.rad(GetCameraAngleZ(0))
-			rayX, rayY, rayZ = U.Rotate3D(rayX, rayY, rayZ, paX, paY, paZ)
-			selectobj[e]=IntersectAll(px,py,pz, px+rayX, py+rayY, pz+rayZ,e)
-			if selectobj[e] ~= 0 or selectobj[e] ~= nil then
-				if g_Entity[e]['obj'] == selectobj[e] then
-					TextCenterOnXColor(50-0.01,50,3,"+",255,255,255) --highliting (with crosshair at present)
-					tEnt[e] = e
-				else 
-					tEnt[e] = 0				
-				end
-			end
-			if selectobj[e] == 0 or selectobj[e] == nil then
-				tEnt[e] = 0
-				TextCenterOnXColor(50-0.01,50,3,"+",155,155,155) --highliting (with crosshair at present)
-			end
-			--end pinpoint select object--
+			--pinpoint select object--
+			module_misclib.pinpoint(e,examine[e].pickup_range,300)
+			tEnt[e] = g_tEnt
+			--end pinpoint select object--	
 		end	
 		if PlayerDist < examine[e].pickup_range and tEnt[e] ~= 0 and GetEntityVisibility(e) == 1 then
-			PromptLocal(e, examine[e].pickup_message)				
+			if examine[e].prompt_display == 1 then PromptLocal(e,examine[e].pickup_message) end
+			if examine[e].prompt_display == 2 then Prompt(examine[e].pickup_message) end
 			if g_KeyPressE == 1 then
 				GravityOff(e)
 				CollisionOff(e)
@@ -119,8 +117,9 @@ function examine_main(e)
 	
 	if status[e] == "examining" then
 		ActivateMouse()
-		PromptLocal(e,examine[e].examine_message)
-		Prompt("WASD or MB1+Move, MMW=Up/Dn, Q to Exit")
+		if examine[e].prompt_display == 1 then PromptLocal(e,examine[e].examine_message) end
+		if examine[e].prompt_display == 2 then Prompt(examine[e].examine_message) end		
+		TextCenterOnX(50,95,3,"WASD or MB1+Move, MMW=Up/Dn, Q to Exit")
 		exminetime[e] = GetElapsedTime() * 100
 		PositionObject(g_Entity[tEnt[e]]['obj'],prop_x[tEnt[e]],prop_y[tEnt[e]]-prop_h[e]/2,prop_z[tEnt[e]])
 		RotateObject(g_Entity[tEnt[e]]['obj'],g_Entity[tEnt[e]]['anglex'],g_Entity[tEnt[e]]['angley'],g_Entity[tEnt[e]]['anglez'])	

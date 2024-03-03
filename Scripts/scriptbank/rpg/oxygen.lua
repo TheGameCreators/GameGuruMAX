99327605365e6f607bca435e0ed85e680957be9a
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-03 13:54:14 +1100

    Update oxygen.lua

--- Scripts/scriptbank/rpg/oxygen.lua
+++ Scripts/scriptbank/rpg/oxygen.lua
@@ -1,57 +1,88 @@
 -- DESCRIPTION: The object will give the player a health boost if collected.
--- Oxygen v3 - thanks to Necrym59
+-- Oxygen v4 - by Necrym59
 -- DESCRIPTION: The object will give the player a oxygen boost and health boost if collected.
--- DESCRIPTION: Change [PROMPT_TEXT$="E to use"]
+-- DESCRIPTION: [PROMPT_TEXT$="E to use"]
 -- DESCRIPTION: [HEALTH_QUANTITY=10(1,40)]
 -- DESCRIPTION: [OXYGEN_QUANTITY=10(1,80)]
 -- DESCRIPTION: [PICKUP_RANGE=80(1,100)]
 -- DESCRIPTION: [@PICKUP_STYLE=1(1=Automatic, 2=Manual)]
+-- DESCRIPTION: [COLLECTED_TEXT$="Collected oxygen supply"]
+-- DESCRIPTION: [@PROMPT_DISPLAY=1(1=Local,2=Screen)]
 -- DESCRIPTION: Play <Sound0> when the object is picked up by the player.
+
+local module_misclib = require "scriptbank\\module_misclib"
+local U = require "scriptbank\\utillib"
+g_tEnt = {}
 
 local oxygen = {}
 local prompt_text = {}
 local heath_quantity = {}
 local oxygen_quantity = {}
 local pickup_range = {}
-local pickup_style = {}	
+local pickup_style = {}
+local collected_text = {}
+local prompt_display = {}
+local selectobj = {}
+local tEnt = {}
+local currentvalue = {}	
 
-function oxygen_properties(e, prompt_text, heath_quantity, oxygen_quantity, pickup_range, pickup_style)
-	oxygen[e] = g_Entity[e]
+function oxygen_properties(e, prompt_text, heath_quantity, oxygen_quantity, pickup_range, pickup_style, collected_text, prompt_display)
 	oxygen[e].prompt_text = prompt_text
 	oxygen[e].heath_quantity = heath_quantity
 	oxygen[e].oxygen_quantity = oxygen_quantity
 	oxygen[e].pickup_range = pickup_range
-	oxygen[e].pickup_style = pickup_style	
+	oxygen[e].pickup_style = pickup_style
+	oxygen[e].collected_text = collected_text
+	oxygen[e].prompt_display = prompt_display
+	if user_global_affected == nil then user_global_affected = "" end
 end
 
 function oxygen_init(e)
-	oxygen[e] = g_Entity[e]
+	oxygen[e] = {}
 	oxygen[e].prompt_text = "E to use"
 	oxygen[e].heath_quantity = 10
 	oxygen[e].oxygen_quantity = 40
 	oxygen[e].pickup_range = 80
 	oxygen[e].pickup_style = 1
+	oxygen[e].collected_text = "Collected oxygen supply"
+	oxygen[e].prompt_display = 1
+	
+	selectobj[e] = 0
+	tEnt[e] = 0
+	g_tEnt = 0
+	currentvalue[e] = 0
 end
 
 function oxygen_main(e)
-	oxygen[e] = g_Entity[e]
+
 	PlayerDist = GetPlayerDistance(e)
 	if oxygen[e].pickup_style == 1 then
 		if PlayerDist < oxygen[e].pickup_range and g_PlayerHealth > 0 then
+			if oxygen[e].prompt_display == 1 then PromptLocal(e,oxygen[e].collected_text) end
+			if oxygen[e].prompt_display == 2 then PromptDuration(oxygen[e].collected_text,2000) end
 			PlaySound(e,0)
+			PerformLogicConnections(e)
+			ActivateIfUsed(e)
 			SetPlayerHealth(g_PlayerHealth + oxygen[e].heath_quantity)
 			local alreadyhaveair = GetGamePlayerControlDrownTimestamp()-Timer()
 			SetGamePlayerControlDrownTimestamp(Timer()+(oxygen[e].oxygen_quantity*100)+alreadyhaveair)
-			Destroy(e)		
+			Destroy(e)
 		end
 	end
-	if oxygen[e].pickup_style == 2 then
-		local LookingAt = GetPlrLookingAtEx(e,1)
-		if LookingAt == 1 and PlayerDist < oxygen[e].pickup_range then
-			Prompt(oxygen[e].prompt_text)
-			if g_KeyPressE == 1 then				
+	if oxygen[e].pickup_style == 2 and PlayerDist < oxygen[e].pickup_range then
+		--pinpoint select object--
+		module_misclib.pinpoint(e,oxygen[e].pickup_range,300)
+		tEnt[e] = g_tEnt
+		--end pinpoint select object--
+		if PlayerDist < oxygen[e].pickup_range and tEnt[e] ~= 0 then
+			if oxygen[e].prompt_display == 1 then PromptLocal(e,oxygen[e].prompt_text) end
+			if oxygen[e].prompt_display == 2 then Prompt(oxygen[e].prompt_text) end
+			if g_KeyPressE == 1 then
+				if oxygen[e].prompt_display == 1 then PromptLocal(e,oxygen[e].collected_text) end
+				if oxygen[e].prompt_display == 2 then PromptDuration(oxygen[e].collected_text,2000) end			
 				PlaySound(e,0)
 				PerformLogicConnections(e)
+				ActivateIfUsed(e)
 				SetPlayerHealth(g_PlayerHealth + oxygen[e].heath_quantity)
 				local alreadyhaveair = GetGamePlayerControlDrownTimestamp()-Timer()
 				SetGamePlayerControlDrownTimestamp(Timer()+(oxygen[e].oxygen_quantity*100)+alreadyhaveair)
@@ -60,3 +91,4 @@ function oxygen_main(e)
 		end
 	end
 end
+

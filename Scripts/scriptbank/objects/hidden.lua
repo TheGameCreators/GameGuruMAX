8c91eba73023d8fd0c20bdc20038a424e9bc3147
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-12 13:37:03 +1100

    Updates

--- Scripts/scriptbank/objects/hidden.lua
+++ Scripts/scriptbank/objects/hidden.lua
@@ -1,6 +1,7 @@
--- Hidden v1 by Necrym59
--- DESCRIPTION: A global behavior that will hide all the same named objects in-game.
--- DESCRIPTION: Attach to an object Physics=ON.
+-- Hidden v2 by Necrym59
+-- DESCRIPTION: A global behavior that will hide all the same named (dynamic) objects in-game.
+-- DESCRIPTION: All objects to be hidden must be dynamic eg: Physics ON, isimobile ON
+-- DESCRIPTION: Attach to any object. Physics=ON.
 -- DESCRIPTION: [OBJECT_NAME$=""]
 -- DESCRIPTION: Object [@COLLISION_MODE=1(1=On, 2=Off)] on or off.
 
@@ -8,9 +9,7 @@ local lower = string.lower
 local hidden 			= {}
 local object_name 		= {}
 local collision_mode 	= {}
-local hiddenlist 		= {}
 
-local name 				= {}
 local status			= {}
 local objEnt			= {}
 
@@ -20,28 +19,25 @@ function hidden_properties(e, object_name, collision_mode)
 	hidden[e].collision_mode = collision_mode
 end 
 
-function hidden_init_name(e,name)
+function hidden_init_name(e)
 	hidden[e] = {}
-	hidden[e].object_name = name
+	hidden[e].object_name = ""
 	hidden[e].collision_mode = 1
 	status[e] = "init"
 end
 
 function hidden_main(e)
-	hidden[e] = g_Entity[e]
 	
 	if status[e] == "init" then
 		for n = 1, g_EntityElementMax do
 			if n ~= nil and g_Entity[n] ~= nil then
 				if lower(GetEntityName(n)) == hidden[e].object_name then					
-					objEnt[e] = n
 					Hide(n)
 					if hidden[e].collision_mode == 1 then CollisionOn(n) end
 					if hidden[e].collision_mode == 2 then CollisionOff(n) end
 				end
 			end
 		end
-		status[e] = "endinit"
+		status[e] = "endinit"	
 	end
-	
 end

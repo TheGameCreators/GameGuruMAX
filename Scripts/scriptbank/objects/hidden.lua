fae4717546e7cbbe94c8403e204268aed0e20d78
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-13 11:05:44 +1100

    Update hidden.lua

--- Scripts/scriptbank/objects/hidden.lua
+++ Scripts/scriptbank/objects/hidden.lua
@@ -1,9 +1,9 @@
--- Hidden v2 by Necrym59
--- DESCRIPTION: A global behavior that will hide all the same named (dynamic) objects in-game.
--- DESCRIPTION: All objects to be hidden must be dynamic eg: Physics ON, isimobile ON
--- DESCRIPTION: Attach to any object. Physics=ON.
+-- Hidden v3 by Necrym59
+-- DESCRIPTION: A global behavior that will hide all the same named objects in-game.
+-- DESCRIPTION: All objects to be hidden must be set dynamic eg: Physics ON, IsImobile ON
 -- DESCRIPTION: [OBJECT_NAME$=""]
--- DESCRIPTION: Object [@COLLISION_MODE=1(1=On, 2=Off)] on or off.
+-- DESCRIPTION: [@COLLISION_MODE=1(1=On, 2=Off)] on or off.
+-- DESCRIPTION: [@NAVMESH_BLOCK=1(1=On, 2=Off)] on or off.
 
 local lower = string.lower
 local hidden 			= {}
@@ -13,16 +13,18 @@ local collision_mode 	= {}
 local status			= {}
 local objEnt			= {}
 
-function hidden_properties(e, object_name, collision_mode)
+function hidden_properties(e, object_name, collision_mode, navmesh_block)
 	hidden[e] = g_Entity[e]
 	hidden[e].object_name = lower(object_name)
 	hidden[e].collision_mode = collision_mode
+	hidden[e].navmesh_block = navmesh_block
 end 
 
 function hidden_init_name(e)
 	hidden[e] = {}
 	hidden[e].object_name = ""
 	hidden[e].collision_mode = 1
+	hidden[e].navmesh_block = 1
 	status[e] = "init"
 end
 
@@ -34,7 +36,19 @@ function hidden_main(e)
 				if lower(GetEntityName(n)) == hidden[e].object_name then					
 					Hide(n)
 					if hidden[e].collision_mode == 1 then CollisionOn(n) end
-					if hidden[e].collision_mode == 2 then CollisionOff(n) end
+					if hidden[e].collision_mode == 2 then CollisionOff(n) end					
+					if hidden[e].navmesh_block == 1 then
+						local x,y,z = GetEntityPosAng(n)
+						y = RDGetYFromMeshPosition(x,y,z)
+						local xmin, ymin, zmin, xmax, ymax, zmax = GetObjectColBox(g_Entity[n]['obj'])
+						local sx, sy, sz = GetObjectScales(g_Entity[n]['obj'])
+						local w, h, l = (xmax - xmin) * sx, (ymax - ymin) * sy, (zmax - zmin) * sz
+						local sizex = w
+						local sizez = l
+						local angle = GetEntityAngleY(n)
+						local blockmode = 1					
+						RDBlockNavMeshWithShape(x,y,z,w,1,l,angle)
+					end					
 				end
 			end
 		end

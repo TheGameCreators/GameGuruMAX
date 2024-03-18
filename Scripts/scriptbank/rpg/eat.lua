544ebeb69e33c850505fb9bcde67dad92d6de36a
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-19 09:05:42 +1100

    Update eat.lua

--- Scripts/scriptbank/rpg/eat.lua
+++ Scripts/scriptbank/rpg/eat.lua
@@ -10,6 +10,7 @@
 -- DESCRIPTION: [RESPAWNING=0!=0]
 -- DESCRIPTION: [RESPAWN_TIME=1(1,60)] in minutes
 -- DESCRIPTION: [@PROMPT_DISPLAY=1(1=Local,2=Screen)]
+-- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline)]
 -- DESCRIPTION: <Sound0> when consuming.
 -- DESCRIPTION: <Sound1> when poisoned.
 
@@ -27,6 +28,7 @@ local poisoning_effect 		= {}
 local respawning 			= {}
 local respawn_time 			= {}
 local prompt_display 		= {}
+local item_highlight 		= {}
 
 local tEnt 					= {}
 local selectobj 			= {}
@@ -40,16 +42,17 @@ local actioned				= {}
 local respawntime			= {}
 local status 				= {}
 
-function eat_properties(e, prompt_text, quantity, pickup_range, effect, user_global_affected, poisoning_effect, respawning, respawn_time, prompt_display)
+function eat_properties(e, prompt_text, quantity, pickup_range, effect, user_global_affected, poisoning_effect, respawning, respawn_time, prompt_display, item_highlight)
 	eat[e].prompt_text = prompt_text
 	eat[e].quantity = quantity
 	eat[e].pickup_range = pickup_range
 	eat[e].effect = effect
 	eat[e].user_global_affected = user_global_affected
 	eat[e].poisoned = poisoning_effect
-	eat[e].respawning = respawning	
+	eat[e].respawning = respawning
 	eat[e].respawn_time = respawn_time
 	eat[e].prompt_display = prompt_display
+	eat[e].item_highlight = item_highlight
 end
 
 function eat_init(e)
@@ -60,10 +63,11 @@ function eat_init(e)
 	eat[e].effect = 1
 	eat[e].user_global_affected = "MyGlobal"
 	eat[e].poisoned = 0
-	eat[e].respawning = 0	
+	eat[e].respawning = 0
 	eat[e].respawn_time = 10
-	eat[e].prompt_display = 1	
-	
+	eat[e].prompt_display = 1
+	eat[e].item_highlight = 0
+
 	currentvalue[e] = 0
 	addquantity[e] = 0
 	calchealth[e] = 0
@@ -79,27 +83,27 @@ function eat_init(e)
 end
 
 function eat_main(e)
-	
+
 	if status[e] == "init" then
 		if eat[e].effect == 1 then addquantity[e] = 1 end
 		if eat[e].effect == 2 then addquantity[e] = 2 end
 		poisoned[e] = eat[e].quantity * 30
 		status[e] = "endinit"
 	end
-	
-	local PlayerDist = GetPlayerDistance(e)	
-	
+
+	local PlayerDist = GetPlayerDistance(e)
+
 	if PlayerDist < eat[e].pickup_range and GetEntityVisibility(e) == 1 then
 		--pinpoint select object--
-		module_misclib.pinpoint(e,eat[e].pickup_range,300)
+		module_misclib.pinpoint(e,eat[e].pickup_range,eat[e].item_highlight)
 		tEnt[e] = g_tEnt
 		--end pinpoint select object--
-		
+
 		if PlayerDist < eat[e].pickup_range and tEnt[e] ~= 0 and GetEntityVisibility(e) == 1 then
 			if doonce[e] == 0 then
 				if eat[e].prompt_display == 1 then PromptLocal(e,eat[e].prompt_text) end
 				if eat[e].prompt_display == 2 then Prompt(eat[e].prompt_text) end
-			end	
+			end
 			if g_KeyPressE == 1 and pressed[e] == 0 then
 				doonce[e] = 1
 				pressed[e] = 1
@@ -114,55 +118,55 @@ function eat_main(e)
 					actioned[e] = 1
 				end
 				if addquantity[e] == 1 then
-					if eat[e].user_global_affected > "" then 
+					if eat[e].user_global_affected > "" then
 						if _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] ~= nil then currentvalue[e] = _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] end
 						_G["g_UserGlobal['"..eat[e].user_global_affected.."']"] = currentvalue[e] + eat[e].quantity
-						if _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] >= 100 then _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] = 100 end													
+						if _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] >= 100 then _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] = 100 end
 					end
-					CollisionOff(e)			
+					CollisionOff(e)
 					Hide(e)
 					if eat[e].respawning == 1 then respawntime[e] = g_Time + ((eat[e].respawn_time*1000)*60) end
-				end	
+				end
 				if addquantity[e] == 2 then
-					if eat[e].user_global_affected > "" then 
+					if eat[e].user_global_affected > "" then
 						if _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] ~= nil then currentvalue[e] = _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] end
 						_G["g_UserGlobal['"..eat[e].user_global_affected.."']"] = currentvalue[e] - eat[e].quantity
 						if _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] <= 0 then _G["g_UserGlobal['"..eat[e].user_global_affected.."']"] = 0 end
 						if eat[e].poisoned == 1 then
-							CollisionOff(e)			
+							CollisionOff(e)
 							Hide(e)
 							if eat[e].respawning == 1 then respawntime[e] = g_Time + ((eat[e].respawn_time*1000)*60) end
 							status[e] = "poisoned"
 						end
 						if eat[e].poisoned == 0 then
-							CollisionOff(e)			
+							CollisionOff(e)
 							Hide(e)
 							if eat[e].respawning == 1 then respawntime[e] = g_Time + ((eat[e].respawn_time*1000)*60) end
 						end
-					end	
-				end							
+					end
+				end
 			end
 
-		end		
+		end
 	end
 	if status[e] == "poisoned" then
 		PlaySound(e,1)
-		if poisoned[e] > 0 then				
+		if poisoned[e] > 0 then
 			GamePlayerControlSetShakeTrauma(565.0)
 			GamePlayerControlSetShakePeriod(960.00)
-			poisoned[e] = poisoned[e]-1			
+			poisoned[e] = poisoned[e]-1
 		end
-		if poisoned[e] == 0 then			
+		if poisoned[e] == 0 then
 			GamePlayerControlSetShakeTrauma(0.0)
 			GamePlayerControlSetShakePeriod(0.00)
 			StopSound(e,0)
 			StopSound(e,1)
-			CollisionOff(e)			
+			CollisionOff(e)
 			Hide(e)
 			if eat[e].respawning == 1 then respawntime[e] = g_Time + ((eat[e].respawn_time*1000)*60) end
 		end
 	end
-	
+
 	if g_Time > respawntime[e] then
 		if eat[e].respawning == 1 then
 			CollisionOn(e)

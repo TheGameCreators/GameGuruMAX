3f5ae42f05fef492a9a5a0518e88e5d5e0f61163
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-18 13:56:57 +1100

    Script Update

--- Scripts/scriptbank/markers/winzone.lua
+++ Scripts/scriptbank/markers/winzone.lua
@@ -1,12 +1,12 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Winzone v13 by Necrym59 and Lee
+-- Winzone v14 by Necrym59 and Lee
 -- DESCRIPTION: When the player enters this zone, <Sound0> will play and the level is complete.
 -- DESCRIPTION: [NOTES_TEXT$="This winzone takes the user to a new level"]
 -- DESCRIPTION: [ZONEHEIGHT=100(0,1000)]
 -- DESCRIPTION: [SpawnAtStart!=1] if unchecked use a switch or other trigger to spawn this zone
 -- DESCRIPTION: [ResetStates!=0] when entering the new level
 -- DESCRIPTION: [@ENDING_MODE=1(1=None, 2=Image, 3=Video, 4=Video+Image)] Type of end level modes
--- DESCRIPTION: [ENDING_IMAGE$=""] for optional ending image
+-- DESCRIPTION: [ENDING_IMAGEFILE$=""] for optional ending image
 -- DESCRIPTION: <Video Slot> for optional ending video
 -- DESCRIPTION: <Sound0> - In zone Effect Sound
 -- DESCRIPTION: [@GoToLevelMode=1(1=Use Storyboard Logic,2=Go to Specific Level)] controls whether to load the next level in the Storyboard, or a specific level.
@@ -14,22 +14,21 @@
 local winzone 			= {}
 local zoneheight		= {}
 local ending_mode		= {}
-local ending_image		= {}
+local ending_imagefile	= {}
 local spawnatstart		= {}
 local resetstates		= {}
 local status			= {}
 local endimg			= {}
 local endvid			= {}
 	
-function winzone_properties(e, notes_text, zoneheight, spawnatstart, resetstates, ending_mode, ending_image)
-	winzone[e] = g_Entity[e]
+function winzone_properties(e, notes_text, zoneheight, spawnatstart, resetstates, ending_mode, ending_imagefile)
 	winzone[e].zoneheight = zoneheight or 100
 	winzone[e].spawnatstart = spawnatstart
 	winzone[e].resetstates = resetstates
 	if ending_mode == nil then ending_mode = 1 end
 	winzone[e].ending_mode = ending_mode
-	if ending_image == nil then ending_image = "" end
-	winzone[e].ending_image = ending_image	
+	if ending_imagefile == nil then ending_imagefile = "" end
+	winzone[e].ending_imagefile = ending_imagefile	
 end
  
 function winzone_init(e)
@@ -38,7 +37,7 @@ function winzone_init(e)
 	winzone[e].spawnatstart = 1
 	winzone[e].resetstates = 0
 	winzone[e].ending_mode = 1
-	winzone[e].ending_image = ""	
+	winzone[e].ending_imagefile = ""	
 	status[e] = "init"
 	endvid[e] = 0
 end
@@ -47,8 +46,8 @@ function winzone_main(e)
 	if status[e] == "init" then
 		if winzone[e].spawnatstart == 1 then SetActivated(e,1) end
 		if winzone[e].spawnatstart == 0 then SetActivated(e,0) end
-		if winzone[e].ending_image ~= nil then 
-			endimg[e] = CreateSprite(LoadImage(winzone[e].ending_image))
+		if winzone[e].ending_imagefile ~= "" then 
+			endimg[e] = CreateSprite(LoadImage(winzone[e].ending_imagefile))
 			SetSpriteSize(endimg[e],100,100)
 			SetSpriteDepth(endimg[e],1)
 			SetSpritePosition(endimg[e],500,500)
@@ -64,7 +63,7 @@ function winzone_main(e)
 				JumpToLevelIfUsedEx(e,winzone[e].resetstates)
 			end
 			if winzone[e].ending_mode == 2 then 		
-				if winzone[e].ending_image > "" then
+				if winzone[e].ending_imagefile > "" then
 					PasteSpritePosition(endimg[e],0,0)
 					FreezePlayer()
 					Prompt ("Press E to Continue" )
@@ -89,7 +88,7 @@ function winzone_main(e)
 					endvid[e] = 1
 				end
 				if endvid[e] == 1 then
-					if winzone[e].ending_image > "" then
+					if winzone[e].ending_imagefile > "" then
 						PasteSpritePosition(endimg[e],0,0)
 						FreezePlayer()
 						Prompt ("Press E to Continue" )

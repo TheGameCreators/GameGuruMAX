dfdc854aaf9a2bf37e8c55392a62e2d85560663c
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-18 13:57:24 +1100

    Update module_misclib.lua

--- Scripts/scriptbank/module_misclib.lua
+++ Scripts/scriptbank/module_misclib.lua
@@ -1,5 +1,5 @@
 -- DESCRIPTION: A global script that provides utility functions for common tasks.
--- Module_Misclib v11 - Necrym and Lee
+-- Module_Misclib v12 - Necrym59 and Lee
 
 g_module_misclib = {}
 g_tEnt = {}
@@ -8,23 +8,29 @@ local module_misclib = {}
 local U = require "scriptbank\\utillib"
 local selectobj = {}
 
-function module_misclib.pinpoint(e,pickuprange,maxrange)
+function module_misclib.pinpoint(e,pickuprange,highlight)
 	--pinpoint select object--
 	PlayerDist = GetPlayerDistance(e)
 	if PlayerDist < pickuprange then
 		selectobj[e]= U.ObjectPlayerLookingAt(pickuprange)
 		if selectobj[e] ~= 0 or selectobj[e] ~= nil then
 			if g_Entity[e]['obj'] == selectobj[e] then
-				TextCenterOnXColor(50-0.01,50,3,"+",255,255,255) -- Compensated -0.01 position for offset of character font
+				TextCenterOnXColor(50-0.01,50,3,"+",255,255,255) 	-- Cross
+				--TextCenterOnXColor(50,50-0.4,3,".",255,255,255) 	-- Dot
 				g_tEnt = e
+				if highlight == 1 then SetEntityEmissiveStrength(g_tEnt,500) end
+				if highlight == 2 then end -- for future outline option
 			else
-				g_tEnt = 0
+				if highlight == 1 then SetEntityEmissiveStrength(g_tEnt,0) end
+				if highlight == 2 then end -- for future outline option
+				g_tEnt = 0				
 			end
 		end
 		if selectobj[e] == 0 or selectobj[e] == nil then
 			g_tEnt = 0
-			TextCenterOnXColor(50-0.01,50,3,"+",155,155,155) -- Compensated -0.01 position for offset of character font
-		end
+			TextCenterOnXColor(50-0.01,50,3,"+",150,150,150) 	-- Cross
+			--TextCenterOnXColor(50,50-0.4,3,".",150,150,150) 	-- Dot
+		end					
 	end
 	--end pinpoint select object--
 end

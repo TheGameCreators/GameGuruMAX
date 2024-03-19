6742750445f032e9e71ef9abd24ee45e09884dc6
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-20 07:42:49 +1100

    Update heal_spell.lua

--- Scripts/scriptbank/rpg/heal_spell.lua
+++ Scripts/scriptbank/rpg/heal_spell.lua
@@ -1,5 +1,5 @@
 -- DESCRIPTION: When collected can be cast to heal the player.
--- Heal Spell v20
+-- Heal Spell v21
 -- DESCRIPTION: [PROMPT_TEXT$="E to collect Healing Spell"]
 -- DESCRIPTION: [USEAGE_TEXT$="You gain some health"]
 -- DESCRIPTION: [PICKUP_RANGE=80(1,100)]
@@ -10,6 +10,7 @@
 -- DESCRIPTION: [PLAYER_LEVEL=0(0,100))] player level to be able use this spell
 -- DESCRIPTION: [PARTICLE1_NAME$="SpellParticle1"]
 -- DESCRIPTION: [PARTICLE2_NAME$="SpellParticle2"]
+-- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline)]
 -- DESCRIPTION: <Sound0> when effect successful
 -- DESCRIPTION: <Sound1> when effect unsuccessful
 
@@ -30,6 +31,7 @@ local cast_radius 				= {}
 local player_level 				= {}
 local particle1_name 			= {}
 local particle2_name 			= {}
+local item_highlight 			= {}
 
 local cast_timeout 		= {}
 local tAllegiance 		= {}
@@ -46,7 +48,7 @@ local tplayerlevel		= {}
 local healcount 		= {}
 local entaffected 		= {}
 
-function heal_spell_properties(e, prompt_text, useage_text, pickup_range, user_global_affected, mana_cost, cast_value, cast_radius, player_level, particle1_name, particle2_name)
+function heal_spell_properties(e, prompt_text, useage_text, pickup_range, user_global_affected, mana_cost, cast_value, cast_radius, player_level, particle1_name, particle2_name, item_highlight)
 	heal_spell[e] = g_Entity[e]
 	heal_spell[e].prompt_text = prompt_text
 	heal_spell[e].useage_text = useage_text
@@ -58,6 +60,7 @@ function heal_spell_properties(e, prompt_text, useage_text, pickup_range, user_g
 	heal_spell[e].player_level = player_level
 	heal_spell[e].particle1_name = lower(particle1_name)
 	heal_spell[e].particle2_name = lower(particle2_name)
+	heal_spell[e].item_highlight = item_highlight
 end
 
 function heal_spell_init(e)
@@ -74,6 +77,7 @@ function heal_spell_init(e)
 	heal_spell[e].particle2_name = "SpellParticle2"
 	heal_spell[e].particle1_number = 0
 	heal_spell[e].particle2_number = 0
+	heal_spell[e].item_highlight = 0
 	heal_spell[e].cast_timeout = 0	
 	status[e] = "init"
 	tAllegiance[e] = 0
@@ -128,7 +132,7 @@ function heal_spell_main(e)
 		local PlayerDist = GetPlayerDistance(e)
 		if PlayerDist < heal_spell[e].pickup_range then
 			--pinpoint select object--
-			module_misclib.pinpoint(e,heal_spell[e].pickup_range,300)
+			module_misclib.pinpoint(e,heal_spell[e].pickup_range,heal_spell[e].item_highlight)
 			sEnt[e] = g_tEnt
 			--end pinpoint select object--
 		end	

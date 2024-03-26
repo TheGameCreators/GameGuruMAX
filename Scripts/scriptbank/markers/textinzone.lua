144d6983b007f4d322bacc4057dff12172febe94
Steve <95015657+Necrym59@users.noreply.github.com>
2024-03-27 07:37:42 +1100

    Update textinzone.lua

--- Scripts/scriptbank/markers/textinzone.lua
+++ Scripts/scriptbank/markers/textinzone.lua
@@ -1,29 +1,41 @@
--- Textinzone v3
--- DESCRIPTION: While the player is within the zone the [PROMPTTEXT$=""] is displayed on screen. 
+-- Textinzone v5  
+-- DESCRIPTION: [PROMPT_TEXT$=""] is displayed on screen while the player is within the zone 
 -- DESCRIPTION: [ZONEHEIGHT=100] controls how far above the zone the player can be before the zone is not triggered.
--- DESCRIPTION: Set[!ONLYONCE=0] for the text zone to be destroyed after activation.
+-- DESCRIPTION: [!ONLYONCE=0] Set for the text zone to be destroyed after activation.
 -- DESCRIPTION: [SpawnAtStart!=1] if unchecked use a switch or other trigger to spawn this zone
+-- DESCRIPTION: [PERIOD=1] time in seconds to display prompt
 
-local textinzone = {}
-local status = {}
-local wait = {}
-local doonce = {}
+local textinzone 	= {}
+local prompt_text	= {}
+local zoneheight	= {}
+local onlyonce		= {}
+local spawnatstart	= {}
+local period		= {}
 
-function textinzone_properties(e, prompttext, zoneheight, onlyonce, spawnatstart)
-	textinzone[e].prompttext = prompttext
+local status		= {}
+local multiuse		= {}
+local wait			= {}
+local doonce		= {}
+
+function textinzone_properties(e, prompt_text, zoneheight, onlyonce, spawnatstart, period)
+	textinzone[e].prompt_text = prompt_text
 	textinzone[e].zoneheight = zoneheight
 	textinzone[e].onlyonce = onlyonce
 	textinzone[e].spawnatstart = spawnatstart
+	textinzone[e].period = period
 end 
 
 function textinzone_init(e)
 	textinzone[e] = {}
-	textinzone[e].prompttext = ""
+	textinzone[e].prompt_text = ""
 	textinzone[e].zoneheight = 100
 	textinzone[e].onlyonce = 0
 	textinzone[e].spawnatstart = 1
+	textinzone[e].period = 1
+	
 	status[e] = "init"
 	wait[e] = math.huge
+	multiuse[e] = 0
 	doonce[e] = 0
 end
 
@@ -32,23 +44,32 @@ function textinzone_main(e)
 	if status[e] == "init" then
 		if textinzone[e].spawnatstart == 1 then SetActivated(e,1) end
 		if textinzone[e].spawnatstart == 0 then SetActivated(e,0) end
-		status[e] = "endinit"
+		status = "endinit"	
 	end
-
+	
 	if g_Entity[e]['activated'] == 1 then
-		if textinzone[e].zoneheight == nil then textinzone[e].zoneheight = 100 end
-		if textinzone[e].zoneheight ~= nil then
-			if g_Entity[e].plrinzone ==1 and g_PlayerPosY > g_Entity[e]['y'] and g_PlayerPosY < g_Entity[e]['y']+textinzone[e].zoneheight then
-				if doonce[e] == 0 then
-					wait[e] = g_Time + 1000
-					doonce[e] = 1
-				end
-				PerformLogicConnections(e)
-				Prompt(textinzone[e].prompttext)
-				if g_Time > wait[e] then
-					if textinzone[e]['onlyonce'] == 1 then Destroy(e) end
-				end
-			end
+		if g_Entity[e]['plrinzone'] == 1 and multiuse[e] == 0 and g_PlayerPosY > g_Entity[e]['y'] and g_PlayerPosY < g_Entity[e]['y']+textinzone[e].zoneheight then
+			if doonce[e] == 0 then
+				PromptDuration(textinzone[e].prompt_text,textinzone[e].period * 1000)
+				wait[e] = g_Time + textinzone[e].period * 1000
+				doonce[e] = 1
+			end	
 		end
-	end
+		if g_Entity[e]['plrinzone'] == 1 and g_Time > wait[e] and g_PlayerPosY > g_Entity[e]['y'] and g_PlayerPosY < g_Entity[e]['y']+textinzone[e].zoneheight then
+			if textinzone[e].onlyonce == 0 and multiuse[e] == 0 then
+				ActivateIfUsed(e)
+				PerformLogicConnections(e)
+				multiuse[e] = 1
+			end	
+			if textinzone[e].onlyonce == 1 then
+				ActivateIfUsed(e)
+				PerformLogicConnections(e)
+				Destroy(e)
+			end	
+		end
+		if g_Entity[e]['plrinzone'] == 0 and multiuse[e] == 1 then
+			doonce[e] = 0
+			multiuse[e] = 0
+		end
+	end	
 end

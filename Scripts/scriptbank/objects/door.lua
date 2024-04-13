ba0e399d3059791751c39a855c24b175b2445a14
Steve <95015657+Necrym59@users.noreply.github.com>
2024-04-13 18:49:14 +1000

    Update door.lua

--- Scripts/scriptbank/objects/door.lua
+++ Scripts/scriptbank/objects/door.lua
@@ -1,10 +1,11 @@
--- Door v25 by Lee and Necrym59
+-- Door v26 by Lee and Necrym59
 -- DESCRIPTION: Open and closes an 'animating' door when the player is within [Range=70(50,500)],
 -- DESCRIPTION: and when triggered will open the door, play <Sound0> and turn collision off after a delay of [DELAY=1000].
 -- DESCRIPTION: When the door is closed, play <Sound1> is played. You can elect to keep the door [Unlocked!=1], and customize the [LockedText$="Door locked. Find key"].
 -- DESCRIPTION: Select if the door also [CannotClose!=0], and customize the [ToOpenText$="to open door"]
 -- DESCRIPTION: Select if the door can [AutoClose!=0] after a [AutoCloseDelay=5000]
 -- DESCRIPTION: Set [@PROMPT_DISPLAY=2(1=Local,2=Screen)]
+-- DESCRIPTION: [!USE_SWITCH=0]
 
 local module_misclib = require "scriptbank\\module_misclib"
 local U = require "scriptbank\\utillib"
@@ -18,7 +19,7 @@ local tEnt 			= {}
 local selectobj 	= {}
 local prompt_display= {}
 
-function door_properties(e, range, delay, unlocked, lockedtext, cannotclose, toopentext, autoclose, autoclosedelay, prompt_display)
+function door_properties(e, range, delay, unlocked, lockedtext, cannotclose, toopentext, autoclose, autoclosedelay, prompt_display, use_switch)
 	door[e]['range'] = range
 	door[e]['delay'] = delay
 	door[e]['unlocked'] = unlocked
@@ -28,6 +29,7 @@ function door_properties(e, range, delay, unlocked, lockedtext, cannotclose, too
 	door[e]['autoclose'] = autoclose
 	door[e]['autoclosedelay'] = autoclosedelay or 0
 	door[e]['prompt_display'] = prompt_display
+	door[e]['use_switch'] = use_switch or 0
 end
 
 function door_init(e)
@@ -40,7 +42,7 @@ function door_init(e)
 	door_pressed[e] = 0
 	tEnt[e] = 0
 	autodelay[e] = math.huge
-	selectobj[e] = 0
+	selectobj[e] = 0	
 end
 
 function door_main(e)
@@ -74,50 +76,64 @@ function door_main(e)
 		end
 	end
 	
-	local LookingAt = GetPlrLookingAtEx(e,1)
-	if PlayerDist < nRange and GetEntityVisibility(e) == 1 and LookingAt == 1 then
-		--pinpoint select object--
-		module_misclib.pinpoint(e,nRange,0)
-		tEnt[e] = g_tEnt
-		--end pinpoint select object--	
-		if PlayerDist < nRange and GetEntityVisibility(e) == 1 and tEnt[e] ~= 0 then
-			if g_Entity[e]['haskey'] ~= 1 then
-				if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,door[e]['lockedtext']) end
-				if door[e]['prompt_display'] == 2 then Prompt(door[e]['lockedtext']) end					
-			else
-				if door[e]['mode'] == 0 then
-					if 1 then
-						if GetGamePlayerStateXBOX() == 1 then
-							if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,"Press Y button " .. door[e]['toopentext']) end
-							if door[e]['prompt_display'] == 2 then Prompt("Press Y button " .. door[e]['toopentext']) end
-						else
-							if GetHeadTracker() == 1 then
-								if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,"Trigger to " .. door[e]['toopentext']) end
-								if door[e]['prompt_display'] == 2 then Prompt("Trigger to " .. door[e]['toopentext']) end							
-							else
-								if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,"Press E to " .. door[e]['toopentext']) end
-								if door[e]['prompt_display'] == 2 then Prompt("Press E to " .. door[e]['toopentext']) end								
-							end
-						end
-						if g_KeyPressE == 1 and g_Entity[e]['animating'] == 0 and door_pressed[e] == 0 then
-							door[e]['mode'] = 201
-							door_pressed[e] = 1
-							autodelay[e] = g_Time + door[e]['autoclosedelay']
-						end
-					end
+	if door[e]['use_switch'] == 0 then
+		local LookingAt = GetPlrLookingAtEx(e,1)
+		if PlayerDist < nRange and GetEntityVisibility(e) == 1 and LookingAt == 1 then
+			--pinpoint select object--
+			module_misclib.pinpoint(e,nRange,0)
+			tEnt[e] = g_tEnt
+			--end pinpoint select object--	
+			if PlayerDist < nRange and GetEntityVisibility(e) == 1 and tEnt[e] ~= 0 then
+				if g_Entity[e]['haskey'] ~= 1 then
+					if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,door[e]['lockedtext']) end
+					if door[e]['prompt_display'] == 2 then Prompt(door[e]['lockedtext']) end					
 				else
-					if door[e]['mode'] == 1 then
-						if door[e]['cannotclose'] == 0 then
+					if door[e]['mode'] == 0 then
+						if 1 then
+							if GetGamePlayerStateXBOX() == 1 then
+								if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,"Press Y button " .. door[e]['toopentext']) end
+								if door[e]['prompt_display'] == 2 then Prompt("Press Y button " .. door[e]['toopentext']) end
+							else
+								if GetHeadTracker() == 1 then
+									if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,"Trigger to " .. door[e]['toopentext']) end
+									if door[e]['prompt_display'] == 2 then Prompt("Trigger to " .. door[e]['toopentext']) end							
+								else
+									if door[e]['prompt_display'] == 1 then TextCenterOnX(50,52,1,"Press E to " .. door[e]['toopentext']) end
+									if door[e]['prompt_display'] == 2 then Prompt("Press E to " .. door[e]['toopentext']) end								
+								end
+							end
 							if g_KeyPressE == 1 and g_Entity[e]['animating'] == 0 and door_pressed[e] == 0 then
-								door[e]['mode'] = 202
+								door[e]['mode'] = 201
 								door_pressed[e] = 1
+								autodelay[e] = g_Time + door[e]['autoclosedelay']
 							end
 						end
-					end						
-				end				
+					else
+						if door[e]['mode'] == 1 then
+							if door[e]['cannotclose'] == 0 then
+								if g_KeyPressE == 1 and g_Entity[e]['animating'] == 0 and door_pressed[e] == 0 then
+									door[e]['mode'] = 202
+									door_pressed[e] = 1
+								end
+							end
+						end						
+					end				
+				end
 			end
 		end
-	end
+	end	
+
+	if door[e]['use_switch'] == 1 and g_Entity[e]['haskey'] == 1 then
+		if g_Entity[e]['activated'] == 1 and door_pressed[e] == 0 then
+			if door[e]['mode'] == 0 then
+				door[e]['mode'] = 201
+				door_pressed[e] = 1
+				autodelay[e] = g_Time + door[e]['autoclosedelay']
+				SetActivated(e,0)
+			end
+		end	
+	end	
+	
 	if door[e]['autoclose'] == 1 and door[e]['cannotclose'] == 0 and g_Entity[e]['animating'] == 0 then
 		if g_Time > autodelay[e] then
 			door[e]['mode'] = 202
@@ -140,7 +156,7 @@ function door_main(e)
 			CollisionOff(e)
 			door[e]['mode'] = 1
 			door[e]['blocking'] = 2
-		end
+		end		
 	end
 	if door[e]['mode'] == 202 then
 		-- close door trigger

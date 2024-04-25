3b340704ac946051cb270a3dc877c9cb34879a62
Steve <95015657+Necrym59@users.noreply.github.com>
2024-04-25 15:51:15 +1000

    Update lifetap_spell.lua

--- Scripts/scriptbank/rpg/lifetap_spell.lua
+++ Scripts/scriptbank/rpg/lifetap_spell.lua
@@ -1,5 +1,5 @@
 -- DESCRIPTION: When collected can cast Lifetap effect to take health from the target and give to the player.
--- Lifetap Spell v21
+-- Lifetap Spell v22
 -- DESCRIPTION: [PROMPT_TEXT$="E to collect Lifetap Spell, T or RMB to target"]
 -- DESCRIPTION: [USEAGE_TEXT$="You cast Lifetap and gained some health"]
 -- DESCRIPTION: [PICKUP_RANGE=80(1,100)]
@@ -160,7 +160,8 @@ function lifetap_spell_main(e)
 			if tEnt[e] ~= nil then tHealth[e] = g_Entity[tEnt[e]]['health'] end			-- get entity health
 			tAllegiance[e] = GetEntityAllegiance(tEnt[e]) 								-- get the allegiance value for this entity (0-enemy, 1-ally, 2-neutral)
 			if tAllegiance[e] == 0 and tEnt[e] ~= nil then								-- if allegiance = enemy then give option to target
-				tTarget[e] = tEnt[e]													-- entity set as target								
+				tTarget[e] = tEnt[e]													-- entity set as target	
+				SetEntityOutline(tEnt[e],1)					
 			end
 			if tTarget[e] > 0 then
 				TextCenterOnX(50,20,3,"TARGETED")

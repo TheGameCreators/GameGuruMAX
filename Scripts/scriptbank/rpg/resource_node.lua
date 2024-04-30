2594294e14c35a15b4d0699a02f45f5abbbbd79c
Steve <95015657+Necrym59@users.noreply.github.com>
2024-04-30 14:06:44 +1000

    Update resource_node.lua

--- Scripts/scriptbank/rpg/resource_node.lua
+++ Scripts/scriptbank/rpg/resource_node.lua
@@ -3,24 +3,32 @@
 -- DESCRIPTION: Allows to use this object as a resource node to give the player the selected resource item.
 -- DESCRIPTION: [@NODE_TYPE=1(1=Growth, 2=Extraction)]
 -- DESCRIPTION: [NODE_TOOL_NAME$="Any"] Weapon Name (eg: enhanced\ak)
--- DESCRIPTION: [NODE_MAX_QUANTITY=1(1,10)]
--- DESCRIPTION: [NODE_SPAWNTIME=0(0,100)] Minutes
--- DESCRIPTION: [RESOURCE_ENTITY_SPAWNTIME=1(0,100)] Minutes
+-- DESCRIPTION: [NODE_RESPAWN_TIME=0(0,100)] Minutes
+-- DESCRIPTION: [NODE_RESOURCE_QUANTITY=1(1,10)]
+-- DESCRIPTION: [RESOURCE_SPAWN_TIME=1(0,100)] Minutes
 -- DESCRIPTION: [RESOURCE_ENTITY_NAME$=""]
--- DESCRIPTION: [!USE_SCALER=1]
+-- DESCRIPTION: [NODE_USE_RANGE=100(0,200)]
+-- DESCRIPTION: [NODE_USE_PROMPT$="Extract Resources"]
+-- DESCRIPTION: [NODE_TOOL_PROMPT$="Tool required to extract Resources"]
+-- DESCRIPTION: [!NODE_SCALER=1]
+-- DESCRIPTION: [!HIDE_NODE=0]
 -- DESCRIPTION: <Sound0> for harvest/extraction sound
 
 local lower = string.lower
 
-g_ResnodeCollected 				= {}
-local resnode					= {}
-local node_type 				= {}
-local node_tool_name			= {}
-local node_max_quantity			= {}
-local resource_entity_spawntime	= {}
-local resource_entity_name		= {}
-local use_scaler				= {}
-local resource_entity_no		= {}
+local resnode						= {}
+local node_type 					= {}
+local node_tool_name				= {}
+local node_respawn_time				= {}
+local node_resource_quantity		= {}
+local resource_spawn_time			= {}
+local resource_entity_name			= {}
+local node_use_range				= {}
+local node_use_prompt				= {}
+local node_tool_prompt				= {}
+local node_scaler					= {}
+local hide_node						= {}
+local resource_entity_no			= {}
 
 
 local origin_x		= {}
@@ -45,14 +53,18 @@ local starthealth	= {}
 local healthcheck	= {}
 local wait			= {}
 
-function resource_node_properties(e, node_type, node_tool_name, node_max_quantity, node_spawntime, resource_entity_spawntime, resource_entity_name, use_scaler)
+function resource_node_properties(e, node_type, node_tool_name, node_respawn_time, node_resource_quantity, resource_spawn_time, resource_entity_name, node_use_range,  node_use_prompt, node_tool_prompt, node_scaler, hide_node)
 	resnode[e].node_type = node_type
 	resnode[e].node_tool_name = lower(node_tool_name)
-	resnode[e].node_max_quantity = node_max_quantity
-	resnode[e].node_spawntime = node_spawntime
-	resnode[e].resource_entity_spawntime = resource_entity_spawntime
+	resnode[e].node_respawn_time = node_respawn_time
+	resnode[e].node_resource_quantity = node_resource_quantity
+	resnode[e].resource_spawn_time = resource_spawn_time
 	resnode[e].resource_entity_name = lower(resource_entity_name) or ""
-	resnode[e].use_scaler = use_scaler or 1
+	resnode[e].node_use_range = node_use_range
+	resnode[e].node_use_prompt = node_use_prompt
+	resnode[e].node_tool_prompt	= node_tool_prompt
+	resnode[e].node_scaler = node_scaler or 1
+	resnode[e].hide_node = hide_node or 0	
 	resnode[e].resource_entity_no = 0
 end
 
@@ -60,11 +72,15 @@ function resource_node_init(e)
 	resnode[e] = {}
 	resnode[e].node_type = 1
 	resnode[e].node_tool_name = "Any"
-	resnode[e].node_max_quantity = 1
-	resnode[e].node_spawntime = 0
-	resnode[e].resource_entity_spawntime = 1	
+	resnode[e].node_respawn_time = 1
+	resnode[e].node_resource_quantity = 1
+	resnode[e].resource_spawn_time = 1	
 	resnode[e].resource_entity_name = ""
-	resnode[e].use_scaler = 1
+	resnode[e].node_use_range = 100
+	resnode[e].node_use_prompt = "Extract Resources"
+	resnode[e].node_tool_prompt	= "Tool required to extract Resources"
+	resnode[e].node_scaler = 1
+	resnode[e].hide_node = 0	
 	resnode[e].resource_entity_no = 0	
 
 	status[e] = "init"
@@ -80,26 +96,30 @@ function resource_node_init(e)
 	created[e] = 0
 	starthealth[e] = 500
 	healthcheck[e] = starthealth[e]
-	wait[e] = math.huge
+	wait[e] = math.huge	
 end
 
 function resource_node_main(e)
 
 	if status[e] == "init" then
 		if resnode[e].node_tool_name == "" then resnode[e].node_tool_name = "none" end
-		if resnode[e].node_type == 1 then
-			CollisionOff(e)
-			Hide(e)
+		if resnode[e].node_type == 1 then		
+			if resnode[e].hide_node == 0 then
+				CollisionOn(e)
+			end	
+			if resnode[e].hide_node == 1 then
+				CollisionOff(e)
+				Hide(e)
+			end
+			spawntimer[e] = g_Time + 2000
+			spawning[e] = 1			
 			status[e] = "create_resource"
 		end
-		if resnode[e].node_type == 2 then
-			status[e] = "start"
-		end
-		if resnode[e].node_max_quantity > 10 then resnode[e].node_max_quantity = 10 end
-		if resnode[e].resource_entity_spawntime > 60 then resnode[e].resource_entity_spawntime = 60 end
+		if resnode[e].node_resource_quantity > 10 then resnode[e].node_resource_quantity = 10 end
+		if resnode[e].resource_spawn_time > 60 then resnode[e].resource_spawn_time = 60 end
 		if resnode[e].resource_entity_no == 0 then
 			for ee = 1, g_EntityElementMax do
-				if ee ~= nil and e~= ee and g_Entity[ee] ~= nil then
+				if ee ~= nil and g_Entity[ee] ~= nil then
 					if lower(GetEntityName(ee)) == lower(resnode[e].resource_entity_name) then
 						resnode[e].resource_entity_no = ee
 						origin_x[e] = g_Entity[ee]['x']
@@ -110,56 +130,60 @@ function resource_node_main(e)
 						break
 					end
 				end
-			end
-		end
+			end			
+		end		
+		status[e] = "start"
 	end
 	
 	local PlayerDist = GetPlayerDistance(e)	
-	
+
 	if status[e] == "start" then
-		if resnode[e].node_type == 1 then
-			if g_ResnodeCollected ~= 0 and g_ResnodeCollected == newEntn[e] and spawning[e] == 0 then
-				if resnode[e].resource_entity_spawntime == 0 then
+		if resnode[e].node_type == 1 then			
+			if spawning[e] == 0 then
+				if resnode[e].resource_spawn_time == 0 then
 					spawntimer[e] = g_Time + (1 * 1000)*2
 					spawning[e] = 1
 				end			
-				if resnode[e].resource_entity_spawntime > 0 then
-					spawntimer[e] = g_Time + (resnode[e].resource_entity_spawntime * 1000)*60
+				if resnode[e].resource_spawn_time > 0 then
+					spawntimer[e] = g_Time + (resnode[e].resource_spawn_time * 1000)*60
 					spawning[e] = 1
 				end	
 			end
-			if created[e] == resnode[e].node_max_quantity then
-				if resnode[e].node_spawntime == 0 then
+			if created[e] == resnode[e].node_resource_quantity then
+				if resnode[e].node_respawn_time == 0 then					
 					Destroy(e)
 				end
-				if resnode[e].node_spawntime > 0 and spawning[e] == 0 then
-					nodetimer[e] = g_Time + (resnode[e].node_spawntime * 1000)*60
+				if resnode[e].node_respawn_time > 0 and spawning[e] == 0 then
+					nodetimer[e] = g_Time + (resnode[e].node_respawn_time * 1000)*60
 					spawning[e] = 2
 				end
 			end
 		end
 	
 		if resnode[e].node_type == 2 then
-			if setupnode[e] == 0 then
+			if setupnode[e] == 0 then				
 				SetEntityHealth(e,starthealth[e])
-				scaler[e] = scaler[e] - (scaler[e]/resnode[e].node_max_quantity)
+				scaler[e] = 100
 				Scale(e,scaler[e])
 				setupnode[e] = 1
 			end
-			if PlayerDist < 200 then
-				if resnode[e].node_tool_name ~= g_PlayerGunName and resnode[e].node_max_quantity > 0 then PromptLocal(e,"Tool required to extract Resources") end
-				if resnode[e].node_tool_name == g_PlayerGunName or resnode[e].node_tool_name == "any" and created[e] < resnode[e].node_max_quantity then PromptLocal(e,"Extract Resources") end
+			if g_Entity[e]['health'] <= 0 then PromptLocal(e,"") end
+			if PlayerDist < resnode[e].node_use_range and g_Entity[e]['health'] > 0 then
+				if resnode[e].node_tool_name ~= g_PlayerGunName and resnode[e].node_resource_quantity > 0 then PromptLocal(e,resnode[e].node_tool_prompt) end
+				if resnode[e].node_tool_name == g_PlayerGunName or resnode[e].node_tool_name == "any" and created[e] < resnode[e].node_resource_quantity then PromptLocal(e,resnode[e].node_use_prompt) end
 			end	
 			if resnode[e].node_tool_name == g_PlayerGunName or resnode[e].node_tool_name == "any" then
 				if g_Entity[e]['health'] < healthcheck[e] then				
 					local drop = math.random(1,2)
 					if drop == 2 then
-						if resnode[e].use_scaler == 1 then
-							scaler[e] = scaler[e] - (scaler[e]/resnode[e].node_max_quantity)
-							Scale(e,scaler[e])
+						if resnode[e].node_scaler == 1 then
+							CollisionOff(e)
+							scaler[e] = scaler[e] - (scaler[e]/resnode[e].node_resource_quantity)
+							Scale(e,scaler[e])							
+							CollisionOn(e)
 						end
 						status[e] = "create_resource"
-						PlaySound(e,0)
+						PlaySound(e,0)						
 						healthcheck[e] = g_Entity[e]['health']
 						if resnode[e].node_tool_name == g_PlayerGunName or resnode[e].node_tool_name == "any" then
 							SetEntityHealth(e,healthcheck[e])
@@ -169,25 +193,25 @@ function resource_node_main(e)
 					end				
 				end				
 				if g_Entity[e]['health'] <= 0 then
-					if created[e] < resnode[e].node_max_quantity then
+					PromptLocal(e,"")
+					if created[e] < resnode[e].node_resource_quantity then
 						status[e] = "create_resource"
-						healthcheck[e] = 1
-						PromptLocal(e,"")
+						healthcheck[e] = 1						
 						SetEntityHealth(e,healthcheck[e])
 					end
 				end
-				if created[e] == resnode[e].node_max_quantity then
+				if created[e] == resnode[e].node_resource_quantity then
+					PromptLocal(e,"")
 					healthcheck[e] = 0
 					SetEntityHealth(e,healthcheck[e])
-					if resnode[e].node_spawntime == 0 then
-						PromptLocal(e,"")
+					if resnode[e].node_respawn_time == 0 then						
 						PerformLogicConnections(e)
 						CollisionOff(e)
-						Hide(e)						
+						Hide(e)
 						Destroy(e)
 					end
-					if resnode[e].node_spawntime > 0 and spawning[e] == 0 then
-						nodetimer[e] = g_Time + (resnode[e].node_spawntime * 1000)*60
+					if resnode[e].node_respawn_time > 0 and spawning[e] == 0 then
+						nodetimer[e] = g_Time + (resnode[e].node_respawn_time * 1000)*60
 						CollisionOff(e)
 						Hide(e)
 						spawning[e] = 2
@@ -198,7 +222,7 @@ function resource_node_main(e)
 	end	
 
 	if status[e] == "create_resource" and resnode[e].resource_entity_no ~= 0 then			
-		if doonce[e] == 0 and created[e] < resnode[e].node_max_quantity then			
+		if doonce[e] == 0 and created[e] < resnode[e].node_resource_quantity then			
 			local etoclone = resnode[e].resource_entity_no
 			newEntn[e] = SpawnNewEntity(etoclone)			
 			Show(newEntn[e])
@@ -221,7 +245,6 @@ function resource_node_main(e)
 			GravityOn(newEntn[e])
 			cntEntn[e] = cntEntn[e] + 1
 			created[e] = created[e] +1	
-			g_ResnodeCollected = 0
 			wait[e] = g_Time + 600			
 			doonce[e] = 1
 		end
@@ -230,7 +253,7 @@ function resource_node_main(e)
 	
 	if status[e] == "resource_created" then
 		doonce[e] = 0
-		status[e] = "start"				
+		status[e] = "start"	
 	end	
 	
 	if g_Time > spawntimer[e] and spawning[e] == 1 then
@@ -241,7 +264,7 @@ function resource_node_main(e)
 		SetEntityHealth(e,starthealth[e])
 		healthcheck[e] = starthealth[e]
 		Scale(e,100)
-		scaler[e] = 100	
+		scaler[e] = 100
 		created[e] = 0		
 		if resnode[e].node_type == 1 then
 			CollisionOff(e)
@@ -253,6 +276,7 @@ function resource_node_main(e)
 			CollisionOn(e)
 			Show(e)
 			spawning[e] = 0
+			setupnode[e] = 0
 			status[e] = "start"
 		end
 	end

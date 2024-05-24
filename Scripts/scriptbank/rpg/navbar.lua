0ac1b1f96acff7d72af36df325c921127375dcfa
Steve <95015657+Necrym59@users.noreply.github.com>
2024-05-24 10:00:06 +1000

    Update navbar.lua

--- Scripts/scriptbank/rpg/navbar.lua
+++ Scripts/scriptbank/rpg/navbar.lua
@@ -1,4 +1,4 @@
--- Navigation Bar v10 by Smallg and Necrym59
+-- Navigation Bar v9 by Smallg and Necrym59
 -- DESCRIPTION: Adds a Nagigation Bar to your game 
 -- DESCRIPTION: [IMAGEFILE$="imagebank\\navbar\\navbar.png"]
 -- DESCRIPTION: [VIEWRANGE=6000] of objectives shown on the radar
@@ -17,7 +17,7 @@
 -- DESCRIPTION: [COMPASS_B=255(0,255)] compass blue color value
 -- DESCRIPTION: [COMPASS_FONT_SIZE=2(1,5)] compass font size
 -- DESCRIPTION: [@OBJECTIVE_DATA=1(1=On,2=Off)] objective data on/off
--- DESCRIPTION: [COMPASS_SPACER$="."] compass spacer character
+--
 
 U = require "scriptbank\\utillib"
 
@@ -39,15 +39,16 @@ local compass_r			= {}
 local compass_g			= {}
 local compass_b			= {}
 local compass_font_size = {}
-local objective_data	= {}
-local compass_spacer 	= {}
 
 local compass			= {}
 local questentity 		= {}
 local queststatus 		= {}
 local status			= ""
+local hudsoff = hudsoff or _G.HideHuds
+local hudson = hudson or _G.ShowHuds
+local hudsShowing = true
 
-function navbar_properties(e, navbarimage, viewrange, width, height, position_x, position_y, icon_width, icon_height, icon_position_y, compass_mode, compass_y, compass_view, compass_r, compass_g, compass_b, compass_font_size, objective_data, compass_spacer)
+function navbar_properties(e, navbarimage, viewrange, width, height, position_x, position_y, icon_width, icon_height, icon_position_y, compass_mode, compass_y, compass_view, compass_r, compass_g, compass_b, compass_font_size, objective_data)
 	nbar.navbarimage = navbarimage or imagefile
 	nbar.viewrange = viewrange
 	nbar.width = width
@@ -65,7 +66,6 @@ function navbar_properties(e, navbarimage, viewrange, width, height, position_x,
 	nbar.compass_b = compass_b
 	nbar.compass_font_size = compass_font_size
 	nbar.objective_data = objective_data
-	nbar.compass_spacer = compass_spacer
 end 
 
 function navbar_init(e)
@@ -88,7 +88,6 @@ function navbar_init(e)
 	nbar.compass_b = 255
 	nbar.compass_font_size = 2
 	nbar.objective_data = 1
-	nbar.compass_spacer = "."	
 	queststatus[e] = 0
 	questentity[e] = 0
 	status = "init"
@@ -111,8 +110,14 @@ function navbar_main(e)
 		angle = angle + (-90.0*0.0174533)
 		return angle
 	end
-	
-	PasteSpritePosition(navbarsprite,nbar.position_x,nbar.position_y)
+	if hudsShowing == false then
+		PasteSpritePosition(20000,20000)
+		nbar.compass_mode = 0
+	end
+	if hudsShowing == true then
+		PasteSpritePosition(navbarsprite,nbar.position_x,nbar.position_y)
+		nbar.compass_mode = 1
+	end
 	if nbar.compass_mode == 1 then show_Compass() end
 	
 	if g_UserGlobalQuestTitleActiveE > 0 and queststatus[e] == 0 then
@@ -153,7 +158,14 @@ function navbar_main(e)
 					SetSpriteSize(iconsprite[b], iw,ih)
 					SetSpriteOffset(iconsprite[b], iw/2, ih/2)
 				end
-				PasteSpritePosition(iconsprite[b],iconx,icony+nbar.icon_position_y)
+				if hudsShowing == false then
+					PasteSpritePosition(iconsprite[b],1000,1000)
+					nbar.objective_data = 0
+				end
+				if hudsShowing == true then
+					PasteSpritePosition(iconsprite[b],iconx,icony+nbar.icon_position_y)
+					nbar.objective_data = 1
+				end	
 				if nbar.objective_data == 1 then 
 					local infoposy = nbar.icon_position_y+2.5
 					TextCenterOnX(iconx,icony+infoposy,2,thisname[b])
@@ -163,6 +175,16 @@ function navbar_main(e)
 			end
 		end
 	end
+end 
+
+function HideHuds()
+	hudsShowing = false
+	hudsoff()
+end
+
+function ShowHuds()
+	hudsShowing = true
+	hudson()
 end 
 
 function AddToNavbar(ee, imgname, fixsize, ignorerng, tname)
@@ -239,7 +261,7 @@ function defn_Compass()
 		elseif i == 271 then compass[i] = "W"  
 		elseif i == 316 then compass[i] = "NW"
 		elseif math.fmod(i,10) == 1 then
-			compass[i] = nbar.compass_spacer
+			compass[i] = "." 
 		else
 			compass[i] = " "
 		end

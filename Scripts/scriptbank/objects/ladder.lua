c2d4a8e277c31ea85dcf225dcf870a341244cc4a
Steve <95015657+Necrym59@users.noreply.github.com>
2024-05-25 15:40:07 +1000

    Update ladder.lua

--- Scripts/scriptbank/objects/ladder.lua
+++ Scripts/scriptbank/objects/ladder.lua
@@ -1,5 +1,5 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Ladder v24
+-- Ladder v25
 -- DESCRIPTION: This script is to be attached to the ladder object? Set ladder objects 
 -- DESCRIPTION: Static mode = Physics on, Collision = Box. Turn ON Object Uses physics, IsImobile.
 -- DESCRIPTION: Turn the default [USE_PROMPT!=1] on/off, change the [PROMPT_TEXT$="W-up S-down, Spacebar-jump off"]. 
@@ -16,39 +16,38 @@
 	local rad = math.rad
 	local abs = math.abs
 
-	local ladders = {}
-	
+	local ladders = {}	
 	local playerheight = 38	
     local playerwidth  = 30
 	local last_gun     = g_PlayerGunName
 	
 function ladder_properties(e, use_prompt ,prompt_text, mount_key, mount_prompt, climbing_speed, grip_adjustment, climb_style, exit_force)
-	ladders[e].use_prompt      = use_prompt
-	ladders[e].prompt_text     = prompt_text
-	ladders[e].mount_key       = mount_key
-	ladders[e].mount_prompt    = mount_prompt
-	ladders[e].climbing_speed  = climbing_speed  or 0
+	ladders[e].use_prompt = use_prompt
+	ladders[e].prompt_text = prompt_text
+	ladders[e].mount_key = mount_key
+	ladders[e].mount_prompt = mount_prompt
+	ladders[e].climbing_speed = climbing_speed  or 0
 	ladders[e].grip_adjustment = grip_adjustment or 1
-	ladders[e].climb_style     = climb_style     or 2
-	ladders[e].exit_force      = exit_force		 or 0.5
+	ladders[e].climb_style = climb_style or 2
+	ladders[e].exit_force = exit_force or 0.5
 end 
 
 function ladder_init(e)
 	ladders[e] = { state = 'init' }
-
-	ladders[e].use_prompt      = 1
-	ladders[e].prompt_text     = ""
-	ladders[e].mount_key       = 0
-	ladders[e].mount_prompt    = "" 
-	ladders[e].climbing_speed  = 0
+	ladders[e].use_prompt = 1
+	ladders[e].prompt_text = ""
+	ladders[e].mount_key = 0
+	ladders[e].mount_prompt = "" 
+	ladders[e].climbing_speed = 0
 	ladders[e].grip_adjustment = 1
-	ladders[e].climb_style     = 2
-	ladders[e].exit_force	   = 0.5
-	ladders[e].playerwidth     = playerwidth
+	ladders[e].climb_style = 2
+	ladders[e].exit_force = 0.5
+	ladders[e].playerwidth = playerwidth
 end -- End init function
 
 function ladder_main(e)
-	local ladder = ladders[e]	
+	local ladder = ladders[e]
+	local LookingAt = GetPlrLookingAtEx(e,1)	
 	-- Ladder init ----------------------------------------------------------------
 	if ladder.state == 'init' then
 		local Ent = g_Entity[e]
@@ -60,47 +59,47 @@ function ladder_main(e)
 		ladder.x = x + xo
 		ladder.y = y
 		ladder.z = z + zo
-		ladder.playerwidth = ladder.playerwidth + ladder.grip_adjustment
-			
-		ladder.state = 'idle'
-		
+		ladder.playerwidth = ladder.playerwidth + ladder.grip_adjustment			
+		ladder.state = 'idle'		
 	-- Ladder idle ----------------------------------------------------------------
 	elseif		
-		ladder.state == 'idle' then		
-		if U.PlayerCloserThanPos( ladder.x, g_PlayerPosY, ladder.z, ladder.playerwidth ) then
-   
-			if ladder.use_prompt == 0 then 
-				Prompt("")
-			elseif
-			   ladder.use_prompt == 1 then 
-				Prompt( ladder.prompt_text ) 
-			end
-			local climbkeyscan = 17
-			if ladder.mount_key == 1 then 
-				Prompt( ladder.mount_prompt )
-				climbkeyscan = 18
-			end
-			
-			local playeraty = g_PlayerPosY - playerheight 
-			if GetGamePlayerControlInWaterState()== 0 then
-				if g_Scancode == climbkeyscan and abs(playeraty-ladder.bottom) < playerheight then 	-- 'W' or alternate 'E' up				
-					ladder.state = 'active'
-					last_gun     = g_PlayerGunName
-					ladder.ypos  = ladder.bottom 			
+		ladder.state == 'idle' then
+
+		if U.PlayerCloserThanPos(ladder.x, g_PlayerPosY, ladder.z, ladder.playerwidth) then
+			if g_PlayerPosY < ladder.top and U.PlayerLookingAt(e,100) == true or g_PlayerPosY >= ladder.top then
+				if ladder.use_prompt == 0 then 
+					Prompt("")
 				elseif
-					g_Scancode == 31 and abs(playeraty-ladder.top) < playerheight then				-- 'S' down
-					ladder.state = 'active'
-					last_gun     = g_PlayerGunName
-					ladder.ypos  = ladder.top
-				end				
-			end	
-			if GetGamePlayerControlInWaterState()>= 1 then
-				if g_Scancode == climbkeyscan then 	-- 'W' or alternate 'E' up				
-					ladder.state = 'active'
-					last_gun     = g_PlayerGunName
-					ladder.ypos  = g_PlayerPosY
+				   ladder.use_prompt == 1 then 
+					Prompt( ladder.prompt_text ) 
 				end
-			end			
+				local climbkeyscan = 17
+				if ladder.mount_key == 1 then 
+					Prompt( ladder.mount_prompt )
+					climbkeyscan = 18
+				end
+				
+				local playeraty = g_PlayerPosY - playerheight 
+				if GetGamePlayerControlInWaterState()== 0 then
+					if g_Scancode == climbkeyscan and abs(playeraty-ladder.bottom) < playerheight then 	-- 'W' or alternate 'E' up				
+						ladder.state = 'active'
+						last_gun     = g_PlayerGunName
+						ladder.ypos  = ladder.bottom 			
+					elseif
+						g_Scancode == 31 and abs(playeraty-ladder.top) < playerheight then				-- 'S' down
+						ladder.state = 'active'
+						last_gun     = g_PlayerGunName
+						ladder.ypos  = ladder.top
+					end				
+				end	
+				if GetGamePlayerControlInWaterState()>= 1 then
+					if g_Scancode == climbkeyscan then 	-- 'W' or alternate 'E' up				
+						ladder.state = 'active'
+						last_gun     = g_PlayerGunName
+						ladder.ypos  = g_PlayerPosY
+					end
+				end
+			end
 		end
 				
 	-- Ladder active -------------------------------------------------------------	

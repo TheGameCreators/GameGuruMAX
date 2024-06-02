d62c0e165acec16b1b8d3878b359e544a5baf375
Steve <95015657+Necrym59@users.noreply.github.com>
2024-06-02 16:09:39 +1000

    Update document.lua

--- Scripts/scriptbank/objects/document.lua
+++ Scripts/scriptbank/objects/document.lua
@@ -1,10 +1,10 @@
--- Document v16 - thanks to Necrym59 and Lee
+-- Document v18 - thanks to Necrym59 and Lee
 -- DESCRIPTION: Change the [PICKUP_TEXT$="E to look at document"].
 -- DESCRIPTION: View position [SCREEN_X=25(0,100)] and [SCREEN_Y=10(0,100)]
 -- DESCRIPTION: Set the [SPRITE_SIZE=15(1,100)] percentage.
 -- DESCRIPTION: The document [IMAGEFILE$="imagebank\\documents\\default_doc.png"]
 -- DESCRIPTION: Set the [PICKUP_RANGE=80(1,300)] 
--- DESCRIPTION: Change the [RETURN_TEXT$="Q to return document"].
+-- DESCRIPTION: Change the [USAGE_TEXT$="MB to +-Zoom, MMW to scroll, Q to return document"]
 -- DESCRIPTION: Set [@PROMPT_DISPLAY=1(1=Local,2=Screen)]
 -- DESCRIPTION: Set to [LOCK_SCREEN!=0] when viewing.
 -- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline)]
@@ -19,7 +19,7 @@ g_tEnt = {}
 local document 			= {}
 local pickup_text 		= {}
 local pickup_range 		= {}
-local return_text 		= {}	
+local usage_text 		= {}	
 local screen_x 			= {}
 local screen_y 			= {}
 local sprite_size 		= {}
@@ -36,11 +36,13 @@ local pressed 			= {}
 local played			= {}
 local doonce			= {}
 local sprite 			= {}
+local adjusty			= {}
+local startwidth		= {}
 local tEnt 				= {}
 local selectobj			= {}
 local status 			= {}
 		
-function document_properties(e, pickup_text, screen_x, screen_y, sprite_size, imagefile, pickup_range, return_text, prompt_display, lock_screen, item_highlight)
+function document_properties(e, pickup_text, screen_x, screen_y, sprite_size, imagefile, pickup_range, usage_text, prompt_display, lock_screen, item_highlight)
 	document[e].pickup_text = pickup_text
 	document[e].screen_x = screen_x					
 	document[e].screen_y = screen_y					
@@ -48,8 +50,8 @@ function document_properties(e, pickup_text, screen_x, screen_y, sprite_size, im
 	document[e].imagefile = imagefile
 	if pickup_range == nil then pickup_range = 80 end
 	document[e].pickup_range = pickup_range
-	if return_text == nil then return_text = "" end
-	document[e].return_text = return_text	
+	if usage_text == nil then usage_text = "" end
+	document[e].usage_text = usage_text	
 	if prompt_display == nil then prompt_display = 1 end
 	document[e].prompt_display = prompt_display
 	if lock_screen == nil then lock_screen = 0 end
@@ -62,7 +64,7 @@ function document_init_name(e)
 	document[e] = {}	
 	document[e].pickup_text = "E to look at document"
 	document[e].pickup_range = 80
-	document[e].return_text = "Q to return document"
+	document[e].usage_text = "MB to +-Zoom, MMW to scroll, Q to return document"
 	document[e].screen_x = 0	
 	document[e].screen_y = 0
 	document[e].sprite_size = 1
@@ -78,6 +80,8 @@ function document_init_name(e)
 	imgWidth[e] = 0	
 	imgHeight[e] = 0
 	selectobj[e] = 0
+	startwidth[e] = 0
+	adjusty[e] = 0
 	tEnt[e] = 0	
 	g_tEnt = 0	
 	played[e] = 0
@@ -91,6 +95,16 @@ function document_main(e)
 	
 	if status[e] == "init" then	
 		SetCameraOverride(0)
+		sprite[e] = CreateSprite(LoadImage(document[e].imagefile))
+		imgAspectRatio[e] = GetImageHeight(LoadImage(document[e].imagefile)) / GetImageWidth(LoadImage(document[e].imagefile))
+		imgWidth[e] = document[e].sprite_size
+		imgHeight[e] = imgWidth[e] * imgAspectRatio[e]
+		SetSpriteSize(sprite[e],imgWidth[e],imgHeight[e])
+		SetSpritePosition(sprite[e],500,500)
+		SetSpriteDepth(sprite[e],100)
+		SetSpritePriority(sprite[e],0)
+		SetSpriteOffset(sprite[e],imgWidth[e]/2.0,imgHeight[e]/2.0)
+		startwidth[e] = imgWidth[e]
 		status[e] = "endinit"
 	end	
 	
@@ -99,7 +113,7 @@ function document_main(e)
 		--pinpoint select object--
 		module_misclib.pinpoint(e,document[e].pickup_range,document[e].item_highlight)
 		tEnt[e] = g_tEnt
-		--end pinpoint select object--	
+		--end pinpoint select object--
 	end	
 	
 	if PlayerDist < document[e].pickup_range and tEnt[e] ~= 0 and GetEntityVisibility(e) == 1 then
@@ -116,17 +130,6 @@ function document_main(e)
 				end
 				Hide(e)
 				showing[e] = 1
-				if doonce[e] == 0 then
-					image[e] = LoadImage(document[e].imagefile)
-					sprite[e] = CreateSprite(image[e])
-					imgAspectRatio[e] = GetImageHeight(image[e]) / GetImageWidth(image[e])
-					imgWidth[e] = document[e].sprite_size
-					imgHeight[e] = imgWidth[e] * imgAspectRatio[e]
-					SetSpriteSize(sprite[e],imgWidth[e],imgHeight[e])					
-					doonce[e] = 1
-				end
-				SetSpritePosition(sprite[e],document[e].screen_x,document[e].screen_y)
-				PasteSpritePosition(sprite[e],document[e].screen_x,document[e].screen_y)
 				if document[e].lock_screen == 1 then
 					SetCameraOverride(3)
 					FreezePlayer()
@@ -137,18 +140,39 @@ function document_main(e)
 				end
 				PerformLogicConnections(e)
 			end
-		end	
+		end		
 	end
 	if showing[e] == 1 then
+		if g_MouseClick == 1 then
+			document[e].sprite_size = document[e].sprite_size + 0.1
+			if document[e].sprite_size >= 50 then document[e].sprite_size = 50 end
+		end
+		if g_MouseClick == 2 then
+			document[e].sprite_size = document[e].sprite_size - 0.1
+			if document[e].sprite_size <= startwidth[e] then document[e].sprite_size = startwidth[e] end
+		end
+		if g_MouseWheel < 0 then	
+			adjusty[e] = adjusty[e] + 1
+		elseif g_MouseWheel > 0 then
+			adjusty[e] = adjusty[e] - 1
+		end
+		imgWidth[e] = document[e].sprite_size
+		imgHeight[e] = imgWidth[e] * imgAspectRatio[e]
+		SetSpriteSize(sprite[e],imgWidth[e],imgHeight[e])
+		SetSpriteOffset(sprite[e],imgWidth[e]/2.0,imgHeight[e]/2.0)
+		SetSpritePosition(sprite[e],document[e].screen_x,document[e].screen_y + adjusty[e])
+		PasteSpritePosition(sprite[e],document[e].screen_x,document[e].screen_y + adjusty[e])
 		PromptLocal(e,"")
 		Prompt("")
-		TextCenterOnX(document[e].screen_x+imgWidth[e]/2,document[e].screen_y+(imgHeight[e]+3),1,document[e].return_text)	
-	end	
-	
+		TextCenterOnX(document[e].screen_x,(document[e].screen_y + imgHeight[e]/2+2) + adjusty[e],1,document[e].usage_text)		
+	end
+
 	local legacyhide = 0
-	if document[e].return_text == "" and PlayerDist > document[e].pickup_range then legacyhide = 1 end
+	if document[e].usage_text == "" and PlayerDist > document[e].pickup_range then legacyhide = 1 end
 	if g_KeyPressQ == 1 or legacyhide == 1 then
 		pressed[e] = 0
+		document[e].sprite_size = startwidth[e]
+		adjusty[e] = 0
 		if played[e] == 2 then
 			PlaySound(e,2)
 			played[e] = 0

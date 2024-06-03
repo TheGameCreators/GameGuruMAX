92af9854b80e33a01004d9c3091d985cf5bcee92
Steve <95015657+Necrym59@users.noreply.github.com>
2024-06-03 11:34:02 +1000

    Update inactive_zone.lua

--- Scripts/scriptbank/markers/inactive_zone.lua
+++ Scripts/scriptbank/markers/inactive_zone.lua
@@ -1,5 +1,5 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Inactive Zone v3   by Necrym59
+-- Inactive Zone v4   by Necrym59
 -- DESCRIPTION: An inactive zone, when triggered will activate all linked entities to the zone and removes the zone afterwards.
 -- DESCRIPTION: Can be triggered by another zone or switch.
 -- DESCRIPTION: Enter the [PROMPT_TEXT$="Zone has been activated"]
@@ -10,18 +10,16 @@ local prompt_text 		= {}
 local status			= {}
 
 function inactive_zone_properties(e, prompt_text)
-	inactivezone[e] = g_Entity[e]
 	inactivezone[e].prompt_text = prompt_text
 end
 
 function inactive_zone_init(e)
-	inactivezone[e] = g_Entity[e]
+	inactivezone[e] = {}
 	inactivezone[e].prompt_text = "Zone has been activated"
 	status[e] = "init"
 end
 
 function inactive_zone_main(e)
-	inactivezone[e] = g_Entity[e]
 
 	if status[e] == "init" then
 		SetActivated(e,0)

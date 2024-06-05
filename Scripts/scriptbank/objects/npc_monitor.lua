81c4c00b396273cd9469ff527e9757eb2a7d13a2
Steve <95015657+Necrym59@users.noreply.github.com>
2024-06-05 10:06:36 +1000

    Update npc_monitor.lua

--- Scripts/scriptbank/objects/npc_monitor.lua
+++ Scripts/scriptbank/objects/npc_monitor.lua
@@ -1,14 +1,14 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- NPC Monitor v6 by Necrym59
--- DESCRIPTION: A global behavior that allows a named npc to be
--- DESCRIPTION: health monitored and trigger event(s) or Lose Game or Win game upon death.
--- DESCRIPTION: Attach to an object set AlwaysActive=ON
--- DESCRIPTION: and attach any logic links to this object
--- DESCRIPTION: and/or use ActivateIfUsed field.
+-- NPC Monitor v8 by Necrym59
+-- DESCRIPTION: A global behavior that allows a named npc to be health monitored and trigger event(s) or Lose Game or Win game upon death.
+-- DESCRIPTION: Attach to an object set AlwaysActive=ON, and attach any logic links to this object and/or use ActivateIfUsed field.
 -- DESCRIPTION: [NPC_NAME$=""] to monitor.
 -- DESCRIPTION: [@DEATH_ACTION=1(1=Event Triggers, 2=Lose Game, 3=Win Game)]
--- DESCRIPTION: [@DISPLAY_HEALTH=2(1=Yes, 2=No)]
--- DESCRIPTION: [MONITOR_ACTIVE!=1] if unchecked use a switch or other trigger to activate/decativate this object
+-- DESCRIPTION: [@DISPLAY_HEALTH=2(1=Yes, 2=No)] to display health on npc
+-- DESCRIPTION: [@MONITOR_ACTIVE=1(1=Yes, 2=No)] if No then use a zone or switch to activate this monitor.
+-- DESCRIPTION: [ACTION_DELAY=2(0,100)] seconds delay before activating death action.
+-- DESCRIPTION: [USER_GLOBAL$=""] user global to apply value (eg: MyGlobal).
+-- DESCRIPTION: [USER_GLOBAL_VALUE=100(1,100)] value to apply.
 
 local lower = string.lower
 
@@ -17,17 +17,23 @@ local npc_name			= {}
 local death_action		= {}
 local display_health	= {}
 local monitor_active	= {}
+local action_delay		= {}
+local user_global 		= {}
+local user_global_value	= {}
 
 local pEntn				= {}
 local status			= {}
 local wait				= {}
 
-function npc_monitor_properties(e, npc_name, death_action, display_health, monitor_active)
+function npc_monitor_properties(e, npc_name, death_action, display_health, monitor_active, action_delay, user_global, user_global_value)
 	npc_monitor[e] = g_Entity[e]
 	npc_monitor[e].npc_name = lower(npc_name) or ""
 	npc_monitor[e].death_action = death_action
 	npc_monitor[e].display_health = display_health
-	npc_monitor[e].monitor_active = monitor_active
+	npc_monitor[e].monitor_active = monitor_active or 1
+	npc_monitor[e].action_delay = action_delay or 0	
+	npc_monitor[e].user_global = user_global
+	npc_monitor[e].user_global_value = user_global_value
 end
 
 function npc_monitor_init(e)
@@ -36,8 +42,12 @@ function npc_monitor_init(e)
 	npc_monitor[e].death_action = 1	
 	npc_monitor[e].display_health = 2
 	npc_monitor[e].monitor_active = 1
+	npc_monitor[e].action_delay = 3		
+	npc_monitor[e].user_global = ""
+	npc_monitor[e].user_global_value = 100	
 	status[e] = "init"
 	wait[e] = math.huge
+	actiondelay[e] = math.huge
 	pEntn[e] = 0
 end
 
@@ -46,7 +56,7 @@ function npc_monitor_main(e)
 
 	if status[e] == "init" then
 		if npc_monitor[e].monitor_active == 1 then SetEntityActivated(e,1) end
-		if npc_monitor[e].monitor_active == 0 then SetEntityActivated(e,0) end	
+		if npc_monitor[e].monitor_active == 2 then SetEntityActivated(e,0) end	
 		pEntn[e] = 0
 		if pEntn[e] == 0 then
 			for n = 1, g_EntityElementMax do
@@ -65,28 +75,43 @@ function npc_monitor_main(e)
 	
 		if status[e] == "monitor" then
 			if g_Entity[pEntn[e]].health <= 0 and npc_monitor[e].death_action == 1 then
-				wait[e] = g_Time + 5000
-				ActivateIfUsed(e)
-				PerformLogicConnections(e)
+				wait[e] = g_Time + (npc_monitor[e].action_delay*1000)				
+				if npc_monitor[e].user_global > "" then
+					_G["g_UserGlobal['"..npc_monitor[e].user_global.."']"] = npc_monitor[e].user_global_value
+				end
 				status[e] = "alarm"
 			end
 			if g_Entity[pEntn[e]].health <= 0 and npc_monitor[e].death_action == 2 then
-				LoseGame()
+				wait[e] = g_Time + (npc_monitor[e].action_delay*1000)
+				status[e] = "winorlose"
 			end
-			if g_Entity[pEntn[e]].health <= 0 and npc_monitor[e].death_action == 3 then
-				WinGame()
+			if g_Entity[pEntn[e]].health <= 0 and npc_monitor[e].death_action == 3 then			
+				wait[e] = g_Time + (npc_monitor[e].action_delay*1000)
+				if npc_monitor[e].user_global > "" then
+					_G["g_UserGlobal['"..npc_monitor[e].user_global.."']"] = npc_monitor[e].user_global_value
+				end
+				status[e] = "winorlose"
 			end
 			if npc_monitor[e].display_health == 1 then
-				TextCenterOnX(50,5,1,"Monitored NPC Health: " ..g_Entity[pEntn[e]].health)
+				PromptLocal(pEntn[e],"Health: " ..g_Entity[pEntn[e]].health)
 			end
 		end
 
-		if status[e] == "alarm" then
+		if status[e] == "alarm" then			
 			if g_Time < wait[e] then MakeAISound(g_PlayerPosX,g_PlayerPosY,g_PlayerPosZ,3000,1,-1) end
 			if g_Time > wait[e] then
+				ActivateIfUsed(e)
+				PerformLogicConnections(e)
 				status[e] = "end"
-				g_Entity[e]['activated'] = 0
 				SwitchScript(e,"no_behavior_selected.lua")
+			end
+		end
+		
+		if status[e] == "winorlose" then
+			if g_Time > wait[e] then
+				if npc_monitor[e].death_action == 2 then LoseGame() end
+				if npc_monitor[e].death_action == 3 then WinGame() end
+				status[e] = "end"
 			end
 		end
 	end

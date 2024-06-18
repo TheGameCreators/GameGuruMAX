ab768cc4aef539a27aafc60ae58dabdb94bd136f
Steve <95015657+Necrym59@users.noreply.github.com>
2024-06-18 11:59:57 +1000

    Update npc_remove.lua

--- Scripts/scriptbank/markers/npc_remove.lua
+++ Scripts/scriptbank/markers/npc_remove.lua
@@ -1,12 +1,11 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Npc Remove v5 by Necrym59
+-- Npc Remove v6 by Necrym59
 -- DESCRIPTION: If npc enters the zone, displays a [NOTIFICATION$="NPC removed"] and will
 -- DESCRIPTION: remove/destroy the NPC, activates any logic links, then destroys this zone.
 -- DESCRIPTION: NPC [@REMOVE_STYLE=1 (1=Instant, 2=Fade)]
 -- DESCRIPTION: Plays <Sound0> when triggered.
 
 local npc_remove 	= {}	
-local npc_type 		= {}
 local notification 	= {}
 local remove_style 	= {}
 local zoneheight	= {}
@@ -18,8 +17,6 @@ local EntityID		= {}
 local status		= {}
 	
 function npc_remove_properties(e, notification, remove_style, zoneheight)
-	npc_remove[e] = g_Entity[e]
-	npc_remove[e].npc_type = npc_type
 	npc_remove[e].notification = notification
 	npc_remove[e].remove_style = remove_style
 	npc_remove[e].zoneheight = zoneheight or 100
@@ -27,7 +24,6 @@ end
 
 function npc_remove_init(e)
 	npc_remove[e] = {}
-	npc_remove[e].npc_type = 0
 	npc_remove[e].notification = "NPC removed"
 	npc_remove[e].remove_style = 1
 	npc_remove[e].zoneheight = 100
@@ -38,11 +34,10 @@ function npc_remove_init(e)
 end
 
 function npc_remove_main(e)
-	npc_remove[e] = g_Entity[e]
 	
-	GetEntityInZone(e)
+	GetEntityInZoneWithFilter(e,2)
 	EntityID[e] = g_Entity[e]['entityinzone']
-	if EntityID[e] ~= 0 or nil and g_Entity[e]['entityinzone'] < g_Entity[e]['y'] + npc_remove[e].zoneheight then
+	if EntityID[e] ~= 0 or nil and g_Entity[EntityID[e]]['y'] > g_Entity[e]['y']-1 and g_Entity[EntityID[e]]['y'] < g_Entity[e]['y'] + npc_remove[e].zoneheight then
 		if doonce[e] == 0 then
 			SetEntityBaseAlpha(EntityID[e],100)
 			SetEntityTransparency(EntityID[e],1)

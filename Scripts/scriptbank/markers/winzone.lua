52dd72d58596da47f1d09833233c1c2a8855eec2
Steve <95015657+Necrym59@users.noreply.github.com>
2024-07-02 07:11:13 +1000

    Update winzone.lua

--- Scripts/scriptbank/markers/winzone.lua
+++ Scripts/scriptbank/markers/winzone.lua
@@ -1,15 +1,16 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Winzone v15 by Necrym59 and Lee
+-- Winzone v16 by Necrym59 and Lee
 -- DESCRIPTION: When the player enters this zone, <Sound0> will play and the level is complete.
 -- DESCRIPTION: [NOTES_TEXT$="This winzone takes the user to a new level"]
 -- DESCRIPTION: [ZONEHEIGHT=100(0,1000)]
 -- DESCRIPTION: [SpawnAtStart!=1] if unchecked use a switch or other trigger to spawn this zone
+-- DESCRIPTION: [ResetStates!=0] when entering the next level
 -- DESCRIPTION: [@ENDING_MODE=1(1=None, 2=Image, 3=Video, 4=Video+Image)] Type of end level modes
 -- DESCRIPTION: [ENDING_IMAGEFILE$=""] for optional ending image
 -- DESCRIPTION: [SPAWN_MARKER_USER_GLOBAL$=""] user global required for using spawn markers (eg: MySpawnMarkers)
--- DESCRIPTION: [SPAWN_MARKER_NAME$=""] name of spawn marker to relocate spawn on next map
+-- DESCRIPTION: [SPAWN_MARKER_NAME$=""] for optional spawning using spawn markers
 -- DESCRIPTION: [@GoToLevelMode=1(1=Use Storyboard Logic,2=Go to Specific Level)] controls whether to load the next level in the Storyboard, or a specific level.
--- DESCRIPTION: [ResetStates!=0] when entering the next level
+
 -- DESCRIPTION: <Video Slot> for optional ending video
 
 local winzone 					= {}
@@ -26,17 +27,17 @@ local status			= {}
 local endimg			= {}
 local endvid			= {}
 	
-function winzone_properties(e, notes_text, zoneheight, spawnatstart, ending_mode, ending_imagefile, spawn_marker_user_global, spawn_marker_name, resetstates)
+function winzone_properties(e, notes_text, zoneheight, spawnatstart, resetstates, ending_mode, ending_imagefile, spawn_marker_user_global, spawn_marker_name)
 	winzone[e].notes_text = notes_text
 	winzone[e].zoneheight = zoneheight or 100
 	winzone[e].spawnatstart = spawnatstart
+	winzone[e].resetstates = resetstates		
 	if ending_mode == nil then ending_mode = 1 end
 	winzone[e].ending_mode = ending_mode
 	if ending_imagefile == nil then ending_imagefile = "" end
 	winzone[e].ending_imagefile = ending_imagefile
 	winzone[e].spawn_marker_user_global = spawn_marker_user_global
 	winzone[e].spawn_marker_name = spawn_marker_name
-	winzone[e].resetstates = resetstates	
 end
  
 function winzone_init(e)
@@ -44,11 +45,11 @@ function winzone_init(e)
 	winzone[e].notes_text = ""
 	winzone[e].zoneheight = 100
 	winzone[e].spawnatstart = 1
+	winzone[e].resetstates = 0		
 	winzone[e].ending_mode = 1
 	winzone[e].ending_imagefile = ""
 	winzone[e].spawn_marker_user_global = ""
 	winzone[e].spawn_marker_name = ""
-	winzone[e].resetstates = 0		
 	
 	status[e] = "init"
 	endvid[e] = 0

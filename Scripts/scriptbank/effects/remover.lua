8a9bd22766a04782baffe33c8505edbfe35323a0
Steve <95015657+Necrym59@users.noreply.github.com>
2025-05-30 12:10:41 +1000

    Update remover.lua
    * NC: Update to remover behavior to stop/remove particles

--- Scripts/scriptbank/effects/remover.lua
+++ Scripts/scriptbank/effects/remover.lua
@@ -1,4 +1,4 @@
--- Remover v3 by Necrym59
+-- Remover v4 by Necrym59
 -- DESCRIPTION: A global behavior that will remove all the same named entities in-game when activated by a switch or zone.
 -- DESCRIPTION: Attach to an object and link to a switch or zone. Set AlwaysActive=ON.
 -- DESCRIPTION: [ENTITY_NAME$=""] Name of entities.
@@ -30,7 +30,8 @@ function remover_main(e)
 	if g_Entity[e]['activated'] == 1 then
 		for n = 1, g_EntityElementMax do
 			if n ~= nil and g_Entity[n] ~= nil then
-				if lower(GetEntityName(n)) == remover[e].entity_name then					
+				if lower(GetEntityName(n)) == remover[e].entity_name then
+					EffectStop(n)
 					Destroy(n)
 				end
 			end

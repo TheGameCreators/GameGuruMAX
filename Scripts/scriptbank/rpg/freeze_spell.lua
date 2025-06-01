8464cd8d4492312bd992ace0a6b540db5cdf5c0f
Steve <95015657+Necrym59@users.noreply.github.com>
2025-06-01 16:13:44 +1000

    Update freeze_spell.lua
    * NC: Update to freeze_spell behavior, user globals selection dropdown

--- Scripts/scriptbank/rpg/freeze_spell.lua
+++ Scripts/scriptbank/rpg/freeze_spell.lua
@@ -1,9 +1,9 @@
 -- DESCRIPTION: When collected can be cast as a Freeze effect to damage the target.
--- Freeze Spell v23
+-- Freeze Spell v24
 -- DESCRIPTION: [PROMPT_TEXT$="E to collect Freeze Spell, T or RMB to target"]
 -- DESCRIPTION: [USEAGE_TEXT$="You cast a Freeze spell"]
 -- DESCRIPTION: [PICKUP_RANGE=80(1,100)]
--- DESCRIPTION: [USER_GLOBAL_AFFECTED$="MyMana"]
+-- DESCRIPTION: [@@USER_GLOBAL_AFFECTED$=""(0=globallist)] eg: MyMana
 -- DESCRIPTION: [MANA_COST=10(1,100)]
 -- DESCRIPTION: [CAST_DAMAGE=25(1,100)]
 -- DESCRIPTION: [CAST_RADIUS=5(1,100))]

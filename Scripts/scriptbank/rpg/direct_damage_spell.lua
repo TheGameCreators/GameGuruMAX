4398df5fb6e0bee0cdb1be526c54f940bfe69ac9
Steve <95015657+Necrym59@users.noreply.github.com>
2025-06-01 15:17:35 +1000

    Update direct_damage_spell.lua
    * NC: Update to direct_damage behavior, user globals selection dropdown

--- Scripts/scriptbank/rpg/direct_damage_spell.lua
+++ Scripts/scriptbank/rpg/direct_damage_spell.lua
@@ -1,9 +1,9 @@
+-- Direct Damage Spell v24
 -- DESCRIPTION: When collected can be cast as a Direct Damage effect to the target.
--- Direct Damage Spell v23
 -- DESCRIPTION: [PROMPT_TEXT$="E to collect Direct Damage Spell, T or RMB to target"]
 -- DESCRIPTION: [USEAGE_TEXT$="Direct Damage Inflicted"]
 -- DESCRIPTION: [PICKUP_RANGE=80(1,100)]
--- DESCRIPTION: [USER_GLOBAL_AFFECTED$="MyMana"]
+-- DESCRIPTION: [@@USER_GLOBAL_AFFECTED$=""(0=globallist)] eg: MyMana
 -- DESCRIPTION: [MANA_COST=10(1,100)]
 -- DESCRIPTION: [CAST_DAMAGE=500(1,100)]
 -- DESCRIPTION: [CAST_RADIUS=5(1,100))]

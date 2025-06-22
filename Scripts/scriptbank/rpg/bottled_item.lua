36f37971dfa42cba79a638ac728432101cb98b21
Steve <95015657+Necrym59@users.noreply.github.com>
2025-06-22 18:42:21 +1000

    Update bottled_item.lua
    * NC: Update to bottled_item behavior, fixes plus highlight option for icon

--- Scripts/scriptbank/rpg/bottled_item.lua
+++ Scripts/scriptbank/rpg/bottled_item.lua
@@ -1,4 +1,4 @@
--- Bottled_Item v10  by Necrym59
+-- Bottled_Item v11  by Necrym59
 -- DESCRIPTION: The object will give the player a health boost or loss if consumed.
 -- DESCRIPTION: and can also effect a user global if required.
 -- DESCRIPTION: [PROMPT_TEXT$="Press E to consume"]
@@ -8,7 +8,8 @@
 -- DESCRIPTION: [@@USER_GLOBAL_AFFECTED$=""(0=globallist)] eg:"MyGlobal"
 -- DESCRIPTION: [POISONING_EFFECT!=0]
 -- DESCRIPTION: [@PROMPT_DISPLAY=1(1=Local,2=Screen)]
--- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline)]
+-- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline,3=Icon)]
+-- DESCRIPTION: [HIGHLIGHT_ICON_IMAGEFILE$="imagebank\\icons\\pickup.png"]
 -- DESCRIPTION: <Sound0> when consuming.
 -- DESCRIPTION: <Sound1> when poisoned.
 
@@ -25,6 +26,7 @@ local poisoning_effect		= {}
 local user_global_affected 	= {}
 local prompt_display		= {}
 local item_highlight 		= {}
+local highlight_icon 		= {}
 
 local tEnt 					= {}
 local selectobj 			= {}
@@ -35,8 +37,11 @@ local poisoned				= {}
 local doonce				= {}
 local actioned				= {}
 local status 				= {}
+local hl_icon 				= {}
+local hl_imgwidth 			= {}
+local hl_imgheight 			= {}
 
-function bottled_item_properties(e, prompt_text, quantity, pickup_range, effect, user_global_affected, poisoning_effect, prompt_display, item_highlight)
+function bottled_item_properties(e, prompt_text, quantity, pickup_range, effect, user_global_affected, poisoning_effect, prompt_display, item_highlight, highlight_icon_imagefile)
 	bottle[e].prompt_text = prompt_text
 	bottle[e].quantity = quantity
 	bottle[e].pickup_range = pickup_range
@@ -45,6 +50,7 @@ function bottled_item_properties(e, prompt_text, quantity, pickup_range, effect,
 	bottle[e].poisoning_effect = poisoning_effect or 0
 	bottle[e].prompt_display = prompt_display
 	bottle[e].item_highlight = item_highlight
+	bottle[e].highlight_icon = highlight_icon_imagefile
 end
 
 function bottled_item_init(e)
@@ -57,6 +63,7 @@ function bottled_item_init(e)
 	bottle[e].poisoning_effect = 0
 	bottle[e].prompt_display = 1
 	bottle[e].item_highlight = 0
+	bottle[e].highlight_icon = "imagebank\\icons\\pickup.png"
 
 	currentvalue[e] = 0
 	addquantity[e] = 0
@@ -68,11 +75,23 @@ function bottled_item_init(e)
 	selectobj[e] = 0
 	actioned[e] = 0
 	status[e] = "init"
+	hl_icon[e] = 0
+	hl_imgwidth[e] = 0
+	hl_imgheight[e] = 0	
 end
 
 function bottled_item_main(e)
 
 	if status[e] == "init" then
+		if bottle[e].item_highlight == 3 and bottle[e].highlight_icon ~= "" then
+			hl_icon[e] = CreateSprite(LoadImage(bottle[e].highlight_icon))
+			hl_imgwidth[e] = GetImageWidth(LoadImage(bottle[e].highlight_icon))
+			hl_imgheight[e] = GetImageHeight(LoadImage(bottle[e].highlight_icon))
+			SetSpriteSize(hl_icon[e],-1,-1)
+			SetSpriteDepth(hl_icon[e],100)
+			SetSpriteOffset(hl_icon[e],hl_imgwidth[e]/2.0, hl_imgheight[e]/2.0)
+			SetSpritePosition(hl_icon[e],500,500)
+		end	
 		if bottle[e].effect == 1 then addquantity[e] = 1 end
 		if bottle[e].effect == 2 then addquantity[e] = 2 end
 		poisoned[e] = bottle[e].quantity * 30
@@ -82,11 +101,11 @@ function bottled_item_main(e)
 	local PlayerDist = GetPlayerDistance(e)
 	if PlayerDist < bottle[e].pickup_range and GetEntityVisibility(e) == 1 then
 		--pinpoint select object--
-		module_misclib.pinpoint(e,bottle[e].pickup_range,bottle[e].item_highlight)
+		module_misclib.pinpoint(e,bottle[e].pickup_range,bottle[e].item_highlight,hl_icon[e])
 		tEnt[e] = g_tEnt
 		--end pinpoint select object--
 
-		if PlayerDist < bottle[e].pickup_range and tEnt[e] ~= 0 and GetEntityVisibility(e) == 1 then
+		if PlayerDist < bottle[e].pickup_range and tEnt[e] == e and GetEntityVisibility(e) == 1 then
 			if doonce[e] == 0 then
 				if bottle[e].prompt_display == 1 then PromptLocal(e,bottle[e].prompt_text) end
 				if bottle[e].prompt_display == 2 then Prompt(bottle[e].prompt_text) end
@@ -94,7 +113,7 @@ function bottled_item_main(e)
 			if g_KeyPressE == 1 then
 				doonce[e] = 1
 				if actioned[e] == 0 then
-					PlaySound(e,0)
+					PlayNon3DSound(e,0)
 					if bottle[e].effect == 1 then calchealth[e] = g_PlayerHealth + bottle[e].quantity end
 					if bottle[e].effect == 2 then calchealth[e] = g_PlayerHealth - bottle[e].quantity end
 					if calchealth[e] > g_gameloop_StartHealth then

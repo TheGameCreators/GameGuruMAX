b7d328af51bbd1487e6224a2ebd4e52fba16a50d
Steve <95015657+Necrym59@users.noreply.github.com>
2025-06-26 16:15:57 +1000

    Update gasmask.lua
    * NC: Update to gasmask behavior, fixes plus highlight option for icon

--- Scripts/scriptbank/rpg/gasmask.lua
+++ Scripts/scriptbank/rpg/gasmask.lua
@@ -1,12 +1,13 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Gasmask v15   by Necrym59
+-- Gasmask v17   by Necrym59
 -- DESCRIPTION: The applied object will give the player a gas protection mask. Set Always active ON.
--- DESCRIPTION: [PICKUP_TEXT$="E to Pickup"] 
+-- DESCRIPTION: [PICKUP_TEXT$="E to Pickup"]
 -- DESCRIPTION: [PICKUP_RANGE=80(1-200)]
 -- DESCRIPTION: [USEAGE_TEXT$="G to wear, Q to remove"]
 -- DESCRIPTION: [IMAGEFILE$="imagebank\\misc\\testimages\\gasmask.png"] for screen overlay
 -- DESCRIPTION: [@PROMPT_DISPLAY=1(1=Local,2=Screen)]
--- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline)]
+-- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline,3=Icon)]
+-- DESCRIPTION: [HIGHLIGHT_ICON_IMAGEFILE$="imagebank\\icons\\pickup.png"]
 -- DESCRIPTION: <Sound0> Put-on Remove Sound
 -- DESCRIPTION: <Sound1> Breathing Sound
 
@@ -23,8 +24,12 @@ local useage_text		= {}
 local screen_image		= {}
 local prompt_display 	= {}
 local item_highlight 	= {}
+local highlight_icon	= {}
 
 local status			= {}
+local hl_icon			= {}
+local hl_imgwidth		= {}
+local hl_imgheight		= {}
 local gasmasksp			= {}
 local gmswitch			= {}
 local have_gasmask		= {}
@@ -32,16 +37,17 @@ local currenthealth		= {}
 local played			= {}
 local selectobj 		= {}
 local tEnt 				= {}
-	
-function gasmask_properties(e, pickup_text, pickup_range, useage_text, screen_image, prompt_display, item_highlight)
+
+function gasmask_properties(e, pickup_text, pickup_range, useage_text, screen_image, prompt_display, item_highlight, highlight_icon_imagefile)
 	gasmask[e].pickup_text = pickup_text
-	gasmask[e].pickup_range = pickup_range	
-	gasmask[e].useage_text = useage_text	
+	gasmask[e].pickup_range = pickup_range
+	gasmask[e].useage_text = useage_text
 	gasmask[e].screen_image = imagefile or screen_image
 	gasmask[e].prompt_display = prompt_display
-	gasmask[e].item_highlight = item_highlight	
-end 
-	
+	gasmask[e].item_highlight = item_highlight
+	gasmask[e].highlight_icon = highlight_icon_imagefile
+end
+
 function gasmask_init(e)
 	gasmask[e] = {}
 	gasmask[e].pickup_text = "E to Pickup"
@@ -49,10 +55,14 @@ function gasmask_init(e)
 	gasmask[e].useage_text = "G to wear, Q to remove"
 	gasmask[e].screen_image = "imagebank\\misc\\testimages\\gasmask.png"
 	gasmask[e].prompt_display = 1
-	gasmask[e].item_highlight = 0	
-	
+	gasmask[e].item_highlight = 0
+	gasmask[e].highlight_icon = "imagebank\\icons\\pickup.png"
+
 	status[e] = "init"
-	currenthealth[e] = 0	
+	hl_icon[e] = 0
+	hl_imgwidth[e] = 0
+	hl_imgheight[e] = 0
+	currenthealth[e] = 0
 	have_gasmask = 0
 	g_gasmask_on = 0
 	played[e] = 0
@@ -61,88 +71,94 @@ function gasmask_init(e)
 	tEnt[e] = 0
 	g_tEnt = 0
 end
- 
+
 function gasmask_main(e)
 
-	if status[e] == "init" then	
+	if status[e] == "init" then
+		if gasmask[e].item_highlight == 3 and gasmask[e].highlight_icon ~= "" then
+			hl_icon[e] = CreateSprite(LoadImage(gasmask[e].highlight_icon))
+			hl_imgwidth[e] = GetImageWidth(LoadImage(gasmask[e].highlight_icon))
+			hl_imgheight[e] = GetImageHeight(LoadImage(gasmask[e].highlight_icon))
+			SetSpriteSize(hl_icon[e],-1,-1)
+			SetSpriteDepth(hl_icon[e],100)
+			SetSpriteOffset(hl_icon[e],hl_imgwidth[e]/2.0, hl_imgheight[e]/2.0)
+			SetSpritePosition(hl_icon[e],500,500)
+		end
 		gasmasksp = CreateSprite(LoadImage(gasmask[e].screen_image))
 		SetSpriteSize(gasmasksp,100,100)
 		SetSpritePosition(gasmasksp,200,200)
 		currenthealth[e] = g_PlayerHealth
 		status = "endinit"
 	end
+
 	local PlayerDist = GetPlayerDistance(e)
-		
+
 	if have_gasmask == 0 then
 		if PlayerDist < gasmask[e].pickup_range and g_PlayerHealth > 0 then
 			--pinpoint select object--
-			module_misclib.pinpoint(e,gasmask[e].pickup_range,gasmask[e].item_highlight)
+			module_misclib.pinpoint(e,gasmask[e].pickup_range,gasmask[e].item_highlight,hl_icon[e])
 			tEnt[e] = g_tEnt
-			--end pinpoint select object--	
-			if PlayerDist < gasmask[e].pickup_range and tEnt[e] ~= 0 then
+			--end pinpoint select object--
+			if PlayerDist < gasmask[e].pickup_range and tEnt[e] == e then
 				if gasmask[e].prompt_display == 1 then PromptLocal(e,gasmask[e].pickup_text) end
-				if gasmask[e].prompt_display == 2 then Prompt(gasmask[e].pickup_text) end		
+				if gasmask[e].prompt_display == 2 then Prompt(gasmask[e].pickup_text) end
 				if g_KeyPressE == 1 then
 					if GetEntityCollectable(tEnt[e]) == 0 then
-						if gasmask[e].prompt_display == 1 then PromptDuration(gasmask[e].useage_text,3000) end
-						if gasmask[e].prompt_display == 2 then PromptDuration(gasmask[e].useage_text,3000) end
-						have_gasmask = 1					
-						PlaySound(e,0)					
+						PromptDuration(gasmask[e].useage_text,3000)
+						have_gasmask = 1
+						PlaySound(e,0)
 						CollisionOff(e)
 						PerformLogicConnections(e)
 						ActivateIfUsed(e)
 						Hide(e)
 					end
 					if GetEntityCollectable(tEnt[e]) == 1 then
-						if gasmask[e].prompt_display == 1 then PromptDuration(gasmask[e].useage_text,3000) end
-						if gasmask[e].prompt_display == 2 then PromptDuration(gasmask[e].useage_text,3000) end
-						have_gasmask = 1					
-						PlaySound(e,0)					
+						PromptDuration(gasmask[e].useage_text,3000)
+						have_gasmask = 1
+						PlaySound(e,0)
 						CollisionOff(e)
 						PerformLogicConnections(e)
 						ActivateIfUsed(e)
 						SetEntityCollected(tEnt[e],1)
 					end
-				end	
+				end
 			end
 		end
 	end
-		
-	if have_gasmask == 1 then	
-		if gmswitch[e] == 0 then 
+
+	if have_gasmask == 1 then
+		SetPosition(e,g_PlayerPosX,g_PlayerPosY+500,g_PlayerPosZ)
+		if gmswitch[e] == 0 then
 			if GetInKey() == "g" or GetInKey() == "G" and gmswitch[e] == 0 then
 				if played[e] == 0 then
-					if gasmask[e].prompt_display == 1 then PromptDuration(gasmask[e].useage_text,3000) end
-					if gasmask[e].prompt_display == 2 then PromptDuration(gasmask[e].useage_text,3000) end
+					PromptDuration(gasmask[e].useage_text,3000)
 					PlaySound(e,0)
 					played[e] = 1
 				end
 				gmswitch[e] = 1
 				g_gasmask_on = 1
-			end	
+			end
 		end
-		if gmswitch[e] == 1 then 	
-			if GetInKey() == "q" or GetInKey() == "Q" and gmswitch[e] == 1 then			
-				if played[e] == 0 then
+		if gmswitch[e] == 1 then
+			if GetInKey() == "q" or GetInKey() == "Q" and gmswitch[e] == 1 then
+				if played[e] == 1 then
 					PlaySound(e,0)
-					played[e] = 1
+					played[e] = 0
 				end
 				gmswitch[e] = 0
 				g_gasmask_on = 0
 			end
 		end
 		if gmswitch[e] == 1 then
-			PasteSpritePosition(gasmasksp,0,0)			
+			PasteSpritePosition(gasmasksp,0,0)
 			LoopSound(e,1)
-			if g_toxiczone == 'gas' then SetPlayerHealth(currenthealth[e]) end						
-			played[e] = 0
+			if g_toxiczone == 'gas' then SetPlayerHealth(currenthealth[e]) end
 		end
-		if gmswitch[e] == 0 then			
-			StopSound(e,1)			
+		if gmswitch[e] == 0 then
+			StopSound(e,1)
 			PasteSpritePosition(gasmasksp,1000,1000)
-			played[e] = 0
 			currenthealth[e] = g_PlayerHealth
 		end
-		if g_PlayerHealth <=0 then have_gasmask = 0 end		
+		if g_PlayerHealth <=0 then have_gasmask = 0 end
 	end
 end
